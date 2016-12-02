d6d72b2e2fc72074e8aa9526fa4d15fcc84c9e31
Vibhav Pant <vibhavp@gmail.com>
2016-12-02 22:52:04 +0530

    Add lsp-pkg.el

---
+++ lsp-pkg.el
@@ -0,0 +1,6 @@
+(define-package "lsp" "0.0.1"
+  "Library for implementing Language Server Protocol clients."
+  '((cl-lib "0.5")
+    (json "1.3")
+    (emacs "25.1"))
+  :url "https://github.com/vibhavp/emacs-lsp")
