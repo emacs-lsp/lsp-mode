7a0b4c3047146381cede0749e6172e69137e80a8
Vibhav Pant <vibhavp@gmail.com>
2016-12-07 22:01:30 +0530

    Fix error messages

--- lsp-send.el
+++ lsp-send.el
@@ -2,7 +2,8 @@
   (process-send-string proc
 		       (concat message))
   (process-send-eof proc)
-  (accept-process-output proc))
+  (with-local-quit
+    (accept-process-output proc)))
 
 (defun lsp--stdin-send-async (message proc)
   (process-send-string proc
