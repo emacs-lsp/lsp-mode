65c326643914a9ce5c6dd535e8ca3a5cdd9ac0f8
Vibhav Pant <vibhavp@gmail.com>
2016-12-07 19:35:56 +0530

    Add racer support

--- lsp-document.el
+++ lsp-document.el
@@ -1,24 +1,23 @@
-(require 'cl)
+(require 'cl-lib)
 (require 'json)
 (require 'xref)
-(require 'lsp-callback)
+(require 'lsp-receive)
 
 (cl-defstruct workspace
   (language-id :read-only t)
   (last-id 0)
   (file-versions)
   (root :ready-only t)
-  (send-sync :read-only t) ;; send-sync should loop until lsp--from-server returns nil
-  (send-async :read-only t)
+  (client :read-only t)
   (data :read-only t))
 
-(defvar-local cur-workspace nil)
-(defvar workspaces (make-hash-table :test 'equal))
+(defvar-local lsp--cur-workspace nil)
+(defvar lsp--workspaces (make-hash-table :test 'equal))
 
 (defun lsp--make-request (method &optional params)
   "Create request body for method METHOD and parameters PARAMS."
   (plist-put (lsp--make-notification method params)
-	     :id (incf (workspace-last-id cur-workspace))))
+	     :id (incf (workspace-last-id lsp--cur-workspace))))
 
 (defun lsp--make-notification (method &optional params)
   "Create notification body for method METHOD and parameters PARAMS."
@@ -36,8 +35,9 @@
 
 (defun lsp--send-notification (body)
   "Send BODY as a notification to the language server."
-  (funcall (workspace-send-async cur-workspace) (lsp--make-message body)
-	   (workspace-data cur-workspace)))
+  (funcall (lsp--client-send-async (workspace-client lsp--cur-workspace))
+	   (lsp--make-message body)
+	   (workspace-data lsp--cur-workspace)))
 
 (defun lsp--send-request (body)
   "Send BODY as a request to the language server, get the response."
@@ -45,13 +45,14 @@
   ;; lsp-send-sync should loop until lsp--from-server returns nil
   ;; in the case of Rust Language Server, this can be done with
   ;; 'accept-process-output`.'
-  (funcall (workspace-send-sync cur-workspace) (lsp--make-message body)
-	   (workspace-data cur-workspace))
-  lsp--response-result)
+  (funcall (lsp--client-send-sync (workspace-client lsp--cur-workspace))
+	   (lsp--make-message body)
+	   (workspace-data lsp--cur-workspace))
+  (prog1 lsp--response-result (setq lsp--response-result nil)))
 
 (defun lsp--cur-file-version (&optional inc)
   "Return the file version number.  If INC, increment it before."
-  (let* ((file-versions (workspace-file-versions cur-workspace))
+  (let* ((file-versions (workspace-file-versions lsp--cur-workspace))
 	 (rev (gethash buffer-file-name file-versions)))
     (when inc
       (incf rev)
@@ -67,42 +68,59 @@ interface TextDocumentItem {
     version: number;
     text: string;
 }"
-  (list :uri buffer-file-name
-	:languageId (workspace-language-id cur-workspace)
-	:version (lsp--cur-file-version)
-	:text (buffer-substring-no-properties (point-min) (point-max))))
+  `(:uri ,(concat "file://" buffer-file-name)
+	 :languageId ,(workspace-language-id lsp--cur-workspace)
+	 :version ,(lsp--cur-file-version)
+	 :text ,(buffer-substring-no-properties (point-min) (point-max))))
 
-(defun lsp--initialize (language-id send-sync send-async &optional data)
-  (let ((cur-dir (expand-file-name default-directory)))
-    (if (gethash cur-dir workspaces)
+(defun lsp--initialize (language-id client &optional data)
+  (let ((root))
+    (if (gethash cur-dir lsp--workspaces)
 	(error "This workspace has already been initialized")
-      (setq cur-workspace (make-workspace :language-id language-id
-					  :file-versions (make-hash-table :test 'equal)
-					  :last-id 0
-					  :root cur-dir
-					  :send-sync send-sync
-					  :send-async send-async
-					  :data data))
-      (puthash cur-dir cur-workspace workspaces))
+      (setq lsp--cur-workspace (make-workspace
+				:language-id language-id
+				:file-versions (make-hash-table :test 'equal)
+				:last-id 0
+				:root (setq root
+					    (funcall (lsp--client-get-root client)))
+				:client client
+				:data data))
+      (puthash cur-dir lsp--cur-workspace lsp--workspaces))
     (lsp--send-request
      (lsp--make-request
       "initialize"
-      `(:processId ,(emacs-pid) :rootPath ,(expand-file-name cur-dir)
-		   :capabilities ,nil)))))
+      `(:processId ,(emacs-pid) :rootPath ,root
+		   :capabilities ,(make-hash-table))))))
 
 (defun lsp--text-document-did-open ()
   "Executed when a new file is opened, added to `find-file-hook'."
   (let ((cur-dir (expand-file-name default-directory))
-	(key))
-    (when (catch 'break
-	    (dolist (key (hash-table-keys workspaces))
+	(key)
+	(client)
+	(data)
+	(set-vars))
+    (if (catch 'break
+	    (dolist (key (hash-table-keys lsp--workspaces))
 	      (when (string-prefix-p key cur-dir)
-		(setq cur-workspace (gethash key workspaces))
+		(setq lsp--cur-workspace (gethash key lsp--workspaces))
 		(throw 'break key))))
-      (puthash buffer-file-name 0 (workspace-file-versions cur-workspace))
-      (lsp--send-notification
-       (lsp--make-notification
-	"textDocument/didOpen" `(:textDocument ,(lsp--make-text-document-item)))))))
+	(progn
+	  (setq set-vars t)
+	  (puthash buffer-file-name 0 (workspace-file-versions lsp--cur-workspace))
+	  (lsp--send-notification
+	   (lsp--make-notification
+	    "textDocument/didOpen"
+	    `(:textDocument ,(lsp--make-text-document-item)))))
+
+      (when (setq client (gethash major-mode lsp--defined-clients))
+	(setq data (funcall (lsp--client-new-connection client)))
+	(setq set-vars t)
+	(lsp--initialize (lsp--client-language-id client)
+			 client
+			 data)
+	(lsp--text-document-did-open)))
+
+    (when set-vars (lsp--set-variables))))
 
 (defun lsp--text-document-identifier ()
   "Make TextDocumentIdentifier.
@@ -110,7 +128,7 @@ interface TextDocumentItem {
 interface TextDocumentIdentifier {
     uri: string;
 }"
-  `(:uri ,buffer-file-name))
+  `(:uri ,(concat "file://" buffer-file-name)))
 
 (defun lsp--versioned-text-document-identifier ()
   "Make VersionedTextDocumentIdentifier.
@@ -136,6 +154,12 @@ interface Position {
   (lsp--position (line-number-at-pos)
 		 (lsp--current-char-offset)))
 
+(defun lsp--point-to-position (point)
+  "Convert POINT to Position."
+  (save-excursion
+    (goto-char point)
+    (lsp--cur-position)))
+
 (defun lsp--position-to-point (params)
   "Convert Position object in PARAMS to a point."
   (save-excursion
@@ -150,7 +174,12 @@ interface Range {
      start: Position;
      end: Position;
  }"
-  (list :start start :end end))
+  `(:start ,start :end ,end))
+
+(defsubst lsp--cur-region-to-range ()
+  "Make Range object for the current region."
+  (lsp--range (lsp--point-to-position (region-beginning))
+	      (lsp--point-to-position (region-end))))
 
 (defun lsp--apply-text-edit (text-edit)
   "Apply the edits described in the TextEdit object in TEXT-EDIT."
@@ -169,28 +198,33 @@ interface Range {
 
 (defun lsp--text-document-did-change (start end length)
   "Executed when a file is changed.
-Added to `after-change-functionsafter-change-functions'"
-  (lsp--cur-file-version t)
-  (lsp--send-notification
-   (lsp--make-notification
-    "textDocument/didChange"
-    `(:textDocument ,(lsp--text-document-identifier) :contentChanges
-		    (,(lsp--text-document-content-change-event start end length))))))
+Added to `after-change-functions'"
+  (when lsp--cur-workspace
+    (lsp--cur-file-version t)
+    (lsp--send-notification
+     (lsp--make-notification
+      "textDocument/didChange"
+      `(:textDocument
+	,(lsp--text-document-identifier)
+	:contentChanges
+	(,(lsp--text-document-content-change-event start end length)))))))
 
 (defun lsp--text-document-did-close ()
   "Executed when the file is closed, added to `kill-buffer-hook'."
-  (remhash buffer-file-name (workspace-file-versions cur-workspace))
-  (lsp--send-notification
-   (lsp--make-notification
-    "textDocument/didClose"
-    `(:textDocument ,(lsp--versioned-text-document-identifier)))))
+  (when lsp--cur-workspace
+    (remhash buffer-file-name (workspace-file-versions lsp--cur-workspace))
+    (lsp--send-notification
+     (lsp--make-notification
+      "textDocument/didClose"
+      `(:textDocument ,(lsp--versioned-text-document-identifier))))))
 
 (defun lsp--text-document-did-save ()
   "Executed when the file is closed, added to `after-save-hook''."
-  (lsp--send-notification
-   (lsp--make-notification
-    "textDocument/didSave"
-    `(:textDocument ,(lsp--versioned-text-document-identifier)))))
+  (when lsp--cur-workspace
+    (lsp--send-notification
+     (lsp--make-notification
+      "textDocument/didSave"
+      `(:textDocument ,(lsp--versioned-text-document-identifier))))))
 
 (defun lsp--text-document-position-params ()
   "Make TextDocumentPositionParams for the current point in the current document."
@@ -212,9 +246,10 @@ Added to `after-change-functionsafter-change-functions'"
   "If RESPONSE is a CompletionItem[], return RESPONSE as is.
 Otherwise return the items field from response, since it would be a
 CompletionList object."
-  (if (consp response)
-      response
-    (gethash "items" response)))
+  (when response
+    (if (consp response)
+	response
+      (gethash "items" response))))
 
 (defun lsp--get-completions ()
   (let ((response (lsp--send-request (lsp--make-request
@@ -244,7 +279,7 @@ interface Location {
     uri: string;
     range: Range;
 }"
-  (let ((uri (gethash location "uri"))
+  (let ((uri (string-remove-prefix "file://" (gethash "uri" location)))
 	(ref-pos (gethash "start" (gethash "range" location))))
     (xref-make uri
 	       (xref-make-file-location uri
@@ -274,7 +309,7 @@ Returns xref-item(s)."
 				       (lsp--make-reference-params)))))
     (if (consp location)
 	(mapcar 'lsp--location-to-xref location)
-      (lsp--location-to-xref location))))
+      (and location (lsp--location-to-xref location)))))
 
 (defun lsp--text-document-hover-string ()
   "interface Hover {
@@ -284,12 +319,12 @@ Returns xref-item(s)."
 
 type MarkedString = string | { language: string; value: string };"
   (let* ((hover (lsp--send-request (lsp--make-request
-			"textDocument/hover"
-			(lsp--text-document-position-params))))
-	 (contents (gethash "contents" hover)))
-    (if (hash-table-p contents)
-	(gethash "value" contents)
-      contents)))
+				       "textDocument/hover"
+				       (lsp--text-document-position-params))))
+	    (contents (gethash "contents" hover)))
+       (if (hash-table-p contents)
+	   (gethash "value" contents)
+	 contents)))
 
 (defun lsp--make-document-formatting-options ()
   (let ((json-false :json-false))
@@ -308,6 +343,64 @@ type MarkedString = string | { language: string; value: string };"
 	(edit))
     (dolist (edit edits)
       (lsp--apply-text-edit edit))))
+
+(defun lsp--make-document-range-formatting-params ()
+  "Make DocumentRangeFormattingParams for selected region.
+interface DocumentRangeFormattingParams {
+    textDocument: TextDocumentIdentifier;
+    range: Range;
+    options: FormattingOptions;
+}" 
+  (plist-put (lsp--make-document-formatting-params)
+	     :range (lsp--cur-region-to-range)))
+
+(defun lsp-format-region (_s _e)
+  (let ((edits (lsp--send-request (lsp--make-request
+				   "textDocument/rangeFormatting"
+				   (lsp--make-document-range-formatting-params))))
+	(edit))
+    (dolist (edit edits)
+      (lsp--apply-text-edit edit))))
+
+(defun lsp--xref-backend () 'lsp)
+
+(cl-defmethod xref-backend-identifier-at-point ((_backend (eql lsp)))
+  (propertize (symbol-name (symbol-at-point))
+	      'def-params (lsp--text-document-position-params)
+	      'ref-params (lsp--make-reference-params)))
+
+(cl-defmethod xref-backend-definitions ((_backend (eql lsp)) identifier)
+  (let* ((properties (text-properties-at 0 identifier))
+	 (params (plist-get properties 'def-params))
+	 (def (lsp--send-request (lsp--make-request
+				  "textDocument/definition"
+				  params))))
+    (if (consp def)
+	(mapcar 'lsp--location-to-xref def)
+      (and def `(,(lsp--location-to-xref def))))))
+
+(cl-defmethod xref-backend-references ((_backend (eql lsp)) identifier)
+  (let* ((properties (text-properties-at 0 identifier))
+	 (params (plist-get properties 'ref-params))
+	 (ref (lsp--send-request (lsp-make-request
+				  "textDocument/references"
+				  params))))
+    (if (consp ref)
+	(mapcar 'lsp--location-to-xref ref)
+      (and ref `(,(lsp--location-to-xref ref))))))
+
+(defalias 'lsp-on-open 'lsp--text-document-did-open)
+(defalias 'lsp-on-save 'lsp--text-document-did-save)
+(defalias 'lsp-on-change 'lsp--text-document-did-change)
+(defalias 'lsp-eldoc 'lsp--text-document-hover-string)
+(defalias 'lsp-completion-at-point 'lsp--get-completions)
+
+(defun lsp--set-variables ()
+  (setq-local eldoc-documentation-function #'lsp-eldoc)
+  (setq-local indent-region-function #'lsp-format-region)
+  (setq-local xref-backend-functions #'lsp--xref-backend)
+  (setq-local completion-at-point-functions nil)
+  (add-hook 'completion-at-point-functions #'lsp-completion-at-point))
 
 (provide 'lsp-document)
 ;;; document.el ends here
