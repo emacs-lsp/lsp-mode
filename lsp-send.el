f8fb604b72f8820b1cfcd324f5b1cdcd29f89711
Vibhav Pant <vibhavp@gmail.com>
2016-12-07 10:10:50 +0530

    add lsp-send

---
+++ lsp-send.el
@@ -0,0 +1,12 @@
+(defun lsp-stdin-send-sync (message proc)
+  (process-send-string proc
+		       (concat message))
+  (process-send-eof proc)
+  (accept-process-output proc))
+
+(defun lsp-stdin-send-async (message proc)
+  (process-send-string proc
+		       (concat message))
+  (process-send-eof proc))
+
+(provide 'lsp-send)
