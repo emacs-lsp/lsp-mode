c24f4d2d7702c9464037536ef3b6a69ab200b382
Vibhav Pant <vibhavp@gmail.com>
2016-12-07 13:10:33 +0530

    Use lsp-- prefix

--- lsp-send.el
+++ lsp-send.el
@@ -1,10 +1,10 @@
-(defun lsp-stdin-send-sync (message proc)
+(defun lsp--stdin-send-sync (message proc)
   (process-send-string proc
 		       (concat message))
   (process-send-eof proc)
   (accept-process-output proc))
 
-(defun lsp-stdin-send-async (message proc)
+(defun lsp--stdin-send-async (message proc)
   (process-send-string proc
 		       (concat message))
   (process-send-eof proc))
