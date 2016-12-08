663cbecd6cc39fc2ae09631ffa9319e48dfd5b70
Vibhav Pant <vibhavp@gmail.com>
2016-12-08 16:42:41 +0530

    Don't send eof

--- lsp-send.el
+++ lsp-send.el
@@ -1,13 +1,11 @@
 (defun lsp--stdin-send-sync (message proc)
   (process-send-string proc
-		       (concat message))
-  (process-send-eof proc)
+		       message)
   (with-local-quit
     (accept-process-output proc)))
 
 (defun lsp--stdin-send-async (message proc)
   (process-send-string proc
-		       (concat message))
-  (process-send-eof proc))
+		       message))
 
 (provide 'lsp-send)
