37a38085092d32207923e5c57a23c228a5e653ed
Vibhav Pant <vibhavp@gmail.com>
2016-12-19 00:24:18 +0530

    Move pos-at-line-col to lsp--position-to-point

--- lsp-common.el
+++ lsp-common.el
@@ -8,16 +8,13 @@
   "Propertize STR as per TYPE."
   (propertize str 'face (alist-get type lsp--message-type-face)))
 
+;; from http://emacs.stackexchange.com/questions/8082/how-to-get-buffer-position-given-line-number-and-column-number
 (defun lsp--position-to-point (params)
   "Convert Position object in PARAMS to a point."
-  (pos-at-line-col (gethash "line" params) (gethash "character" params)))
-
-;; from http://emacs.stackexchange.com/questions/8082/how-to-get-buffer-position-given-line-number-and-column-number
-(defun pos-at-line-col (l c)
   (save-excursion
     (goto-char (point-min))
-    (forward-line l)
-    (move-to-column c)
+    (forward-line (gethash "line" params))
+    (move-to-column (gethash "character" params))
     (point)))
 
 (provide 'lsp-common)
