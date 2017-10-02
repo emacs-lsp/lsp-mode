edafb42260c570d822d4d410bf402886c0acb919
Andrew Stahlman <andrew.stahlman@gmail.com>
2017-10-02 06:50:46 -0700

    Cleanup for review. Add logging to lsp-common.

--- lsp-methods.el
+++ lsp-methods.el
@@ -600,10 +600,9 @@ interface Range {
   (gethash cap (or capabilities (lsp--server-capabilities))))
 
 (defun lsp--get-text-deleted-in-most-recent-change ()
-  "If the most recent change in buffer-undo-list includes a text
-deletion, return the text corresponding to the first
-deletion. Returns nil if the latest change in buffer-undo-list
-does not contain a deletion."
+  "Search until the first undo boundary and return the text
+corresponding to the first deletion in buffer-undo-list, if
+present. Else return nil. "
   (if undo-tree-mode
       (user-error "lsp-mode is not compatible with undo-tree mode!")
     (let ((l buffer-undo-list)
@@ -614,57 +613,32 @@ does not contain a deletion."
           (setq l (cdr l))))
       result)))
 
-
-(defun lsp--substitution-change (start end length)
-  (if-let ((last-deletion-change (lsp--get-text-deleted-in-most-recent-change)))
-      (let* ((d (split-string last-deletion-change "\n"))
-             (start-p (lsp-point-to-position start))
-             (end-p '(:line 0 :character 0)))
-        (plist-put end-p :line (+ (plist-get start-p :line) (length d) -1))
-        (plist-put end-p :character (if (> (length d) 1)
-                                        (length (car (last d)))
-                                      (+ (length (car (last d))) (plist-get start-p :character))))
-        `(:range (:start ,start-p :end ,end-p)
-                 :rangeLength ,length
-                 :text ,(buffer-substring-no-properties start end)))
-    (log DEBUG "Ignoring text change, because it seems like nothing actually changed: [start=%s; end=%s; length=%s]" start end length)))
-
-(defun lsp--delete-change (start end length)
-  (if-let* ((last-deletion-change (lsp--get-text-deleted-in-most-recent-change))
-            (d (split-string  last-deletion-change "\n"))
-            (start-p (lsp-point-to-position start))
-            (end-p '(:line 0 :character 0)))
-      (progn
-        (plist-put end-p :line (+ (plist-get start-p :line) (length d) -1))
-        (plist-put end-p :character (if (> (length d) 1)
-                                        (length (car (last d)))
-                                      (+ (length (car (last d))) (plist-get start-p :character))))
-        `(:range (:start ,start-p
-                         :end ,end-p)
-                 :rangeLength ,length
-                 :text ""))
-    (error "Can't send a change event to the server because we couldn't recover the deleted text. This is likely a bug in lsp-mode")))
-
 (defun lsp--substitution-or-deletion-change (start end length)
+  "Return a change corresponding to a text substitution or
+deletion by recovering the affected text from the undo
+history. This logic doesn't play well with the redo functionality
+provided by undo-tree mode."
   (if-let* ((last-deletion-change (lsp--get-text-deleted-in-most-recent-change))
-            (d (split-string  last-deletion-change "\n"))
+            (deleted-lines (split-string last-deletion-change "\n"))
             (start-p (lsp-point-to-position start))
             (end-p '(:line 0 :character 0)))
       (progn
-        (plist-put end-p :line (+ (plist-get start-p :line) (length d) -1))
-        (plist-put end-p :character (if (> (length d) 1)
-                                        (length (car (last d)))
-                                      (+ (length (car (last d))) (plist-get start-p :character))))
+        (plist-put end-p :line (+ (plist-get start-p :line) (length deleted-lines) -1))
+        (plist-put end-p :character (if (> (length deleted-lines) 1)
+                                        (length (car (last deleted-lines)))
+                                      (+ (length (car (last deleted-lines))) (plist-get start-p :character))))
         `(:range (:start ,start-p :end ,end-p)
                  :rangeLength ,length
                  :text ,(buffer-substring-no-properties start end)))
+    ;; else, we couldn't find a deletion in the undo history
     (if (eq start end)
       ;; deletion
       (error "Can't send a change event to the server because we couldn't recover the deleted text. This is likely a bug in lsp-mode")
-      ;; substitution
-      (log DEBUG "Ignoring text change, because it seems like nothing actually changed: [start=%s; end=%s; length=%s]" start end length))))
-
-
+      ;; substitution - I've observed the undo of a substitution
+      ;; trigger 3 changes, the first of which seems to overwrite part
+      ;; of the affected region with the same text that's already
+      ;; present
+      (lsp--log DEBUG "Ignoring text change, because it seems like nothing actually changed: [start=%s; end=%s; length=%s]" start end length))))
 
 (defun lsp--text-document-content-change-event (start end length)
   "Make a TextDocumentContentChangeEvent body for START to END, of length LENGTH."
@@ -681,17 +655,24 @@ does not contain a deletion."
   ;;            ,"end"  :{"line":7,"character":0}}
   ;;            ,"rangeLength":7
   ;;            ,"text":""}
-  (message "In change hook: start=%s; end=%s; length=%s\nBuffer contents:\n%s\nbuffer-undo-list: %s" start end length
-           (buffer-substring-no-properties (point-min) (point-max))
-           (prin1-to-string buffer-undo-list))
+  (lsp--log DEBUG
+            "In change hook: [start=%s; end=%s; length=%s]
+Buffer contents:
+---------------------------------
+%s
+---------------------------------
+buffer-undo-list: %s"
+            start end length
+            (buffer-substring-no-properties (point-min) (point-max))
+            (prin1-to-string buffer-undo-list))
   (if (eq length 0)
       ;; Adding something, work from start only
       `(:range ,(lsp--range (lsp--point-to-position start)
                             (lsp--point-to-position start))
                :rangeLength 0
                :text ,(buffer-substring-no-properties start end))
-      ;; Deleting or substituting something
-      (lsp--substitution-or-deletion-change start end length)))
+    ;; Deleting or substituting something
+    (lsp--substitution-or-deletion-change start end length)))
 
 ;; Observed from vscode for applying a diff replacing one line with
 ;; another. Emacs on-change shows this as a delete followed by an
@@ -722,15 +703,6 @@ does not contain a deletion."
 (defvar-local lsp--changes [])
 (defvar-local lsp--has-changes []
   "non-nil if the current buffer has any changes yet to be sent.")
-
-(defconst DEBUG 0)
-(defconst INFO 1)
-
-(defvar-local lsp--log-level DEBUG)
-
-(defmacro log (lvl fmt &rest args)
-  `(when (>= ,lvl lsp--log-level)
-     (message ,(format "[%s]: %s" lvl fmt) ,@args)))
 
 (defun lsp--rem-idle-timer ()
   (when lsp--change-idle-timer
