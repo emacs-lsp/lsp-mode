478efaba095ec7f39ff255f73f00be43ae0bcb8d
Vibhav Pant <vibhavp@gmail.com>
2017-12-23 18:16:47 +0530

    Rewrite edits

--- lsp-methods.el
+++ lsp-methods.el
@@ -430,7 +430,8 @@ Return the merged plist."
 
 (defun lsp--client-workspace-capabilities ()
   "Client Workspace capabilities according to LSP."
-  `(:executeCommand (:dynamicRegistration t)))
+  `(:applyEdit t
+     :executeCommand (:dynamicRegistration t)))
 
 (defun lsp--client-textdocument-capabilities ()
   "Client Text document capabilities according to LSP."
@@ -707,25 +708,50 @@ interface Range {
   "Return the end line for a given LSP range, in LSP coordinates"
   (inline-quote (plist-get (plist-get ,range :end) :line)))
 
-(defun lsp--apply-workspace-edits (edits)
-  (cl-letf (((lsp--workspace-change-timer-disabled lsp--cur-workspace) t))
-    (maphash (lambda (key value)
-               (lsp--apply-workspace-edit key value))
-             (gethash "changes" edits))))
+(defun lsp--apply-workspace-edit (edit)
+  "Apply the WorkspaceEdit object EDIT.
 
-(defun lsp--apply-workspace-edit (uri edits)
-  (let ((filename (string-remove-prefix lsp--uri-file-prefix uri)))
-    ;; TODO: What if the buffer has been modified?
-    ;;       Although, for incremental sync that should be fine
-    (when (not (find-buffer-visiting filename))
-      (progn (find-file filename)
-             (lsp--text-document-did-open)))
-    (lsp--apply-text-edits edits)))
+interface WorkspaceEdit {
+	changes?: { [uri: string]: TextEdit[]; };
+	documentChanges?: TextDocumentEdit[];
+}"
+  (let ((changes (gethash "changes" edit))
+         (document-changes (gethash "documentChanges" edit)))
+    (if document-changes
+      (mapc #'lsp--apply-text-document-edit document-changes)
 
-(defun lsp--apply-text-edits (edits)
+      (maphash
+        (lambda (uri text-edits)
+          (let ((filename (string-remove-prefix lsp--uri-file-prefix uri)))
+            (with-current-buffer (find-file-noselect filename)
+              (lsp--apply-text-edits text-edits))))
+        changes))))
+
+(define-inline lsp--apply-workspace-edits (edits)
+  (inline-quote (mapc #'lsp--apply-workspace-edit ,edits)))
+
+(defun lsp--apply-text-document-edit (edit)
+  "Apply the TextDocumentEdit object EDIT.
+If the file is not being visited by any buffer, it is opened with
+`find-file-noselect'.
+Because lsp-mode does not store previous document versions, the edit is only
+applied if the version of the textDocument matches the version of the
+corresponding file.
+
+interface TextDocumentEdit {
+	textDocument: VersionedTextDocumentIdentifier;
+	edits: TextEdit[];
+}"
+  (let* ((ident (gethash "textDocument" edit))
+          (filename (string-remove-prefix lsp--uri-file-prefix (gethash "uri" ident)))
+          (version (gethash "version" ident)))
+    (with-current-buffer (find-file-noselect filename)
+      (when (= version (lsp--cur-file-version))
+        (lsp--apply-text-edits (gethash "edits" edit))))))
+
+(define-inline lsp--apply-text-edits (edits)
   "Apply the edits described in the TextEdit[] object in EDITS."
-  (dolist (edit edits)
-    (lsp--apply-text-edit edit)))
+  (inline-quote (mapc #'lsp--apply-text-edit ,edits)))
 
 (defun lsp--apply-text-edit (text-edit)
   "Apply the edits described in the TextEdit object in TEXT-EDIT."
