e35210e9b2cdd30c99eabc63072263a611c24d7c
Vibhav Pant <vibhavp@gmail.com>
2018-02-15 18:25:46 +0530

    Update copyright years.

--- lsp-mode.el
+++ lsp-mode.el
@@ -1,6 +1,6 @@
 ;;; lsp-mode.el --- Minor mode for interacting with Language Servers -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016-2017  Vibhav Pant <vibhavp@gmail.com>
+;; Copyright (C) 2016-2018  Vibhav Pant <vibhavp@gmail.com>
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
--- test/lsp-io-test.el
+++ test/lsp-io-test.el
@@ -1,6 +1,6 @@
 ;;; lsp-io-test.el --- unit tests for lsp-io.el -*- lexical-binding: t -*-
 
-;; Copyright (C) 2016-2017  Vibhav Pant <vibhavp@gmail.com>.
+;; Copyright (C) 2016-2018  Vibhav Pant <vibhavp@gmail.com>.
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
