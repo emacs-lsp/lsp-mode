e38b2fb3dceefcc3995782d40a7ffbd812a1c7f9
Ivan Yonchovski <yyoncho@gmail.com>
2018-12-20 21:50:56 +0200

    delete lsp-mode.el

--- lsp.el
+++ lsp.el
@@ -5,6 +5,7 @@
 ;; Author: Ivan Yonchovski <ivan.yonchovski@tick42.com>
 ;; Author: Vibhav Pant <vibhavp@gmail.com>
 ;; Keywords: languages
+;; Package-Requires: ((emacs "25.1") (dash "2.14.1") (dash-functional "2.14.1") (f "0.20.0") (ht "2.0") (spinner "1.7.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
