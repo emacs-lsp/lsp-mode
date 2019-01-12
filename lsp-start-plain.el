d3fa2b15c2f698229fd2391c8816deecc7501f32
citreu <all_but_last@163.com>
2019-01-12 13:01:34 +0800

    Add lsp-start-plain.el to provide a quick starter for minimal env.

---
+++ lsp-start-plain.el
@@ -0,0 +1,49 @@
+;;; lsp-start-plain.el --- LSP mode quick starter      -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018 Ivan Yonchovski
+
+;; Author: Zhu Zihao <all_but_last@163.com>
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file is a helper to start a minimal lsp environment.
+;; To use this, start your Emacs with "emacs -q" and load this file.
+
+;; It will install `use-package' and `lsp-mode', `lsp-ui', `company-lsp'
+;; with their depedencies to start a minimal lsp environment.
+
+;; And it forces Emacs to load `.el' files rather than `.elc' files
+;; for more readable backtrace.
+
+;;; Code:
+
+(let ((package-archives '(("melpa" . "http://melpa.org/packages/")
+                          ("gnu" . "http://elpa.gnu.org/packages/")))
+      (lexical-binding t))
+  (setq debug-on-error t)
+  (require 'cl-lib)
+  (require 'package)
+  (package-initialize)
+  (package-refresh-contents)
+  (cl-flet* ((ensure-pkg (pkg)
+                         (unless (package-installed-p pkg)
+                           (package-install pkg))
+                         (let ((load-suffixes '(".el")))
+                           (require pkg))))
+    (ensure-pkg 'lsp-mode)
+    (ensure-pkg 'lsp-ui)
+    (ensure-pkg 'company-lsp)))
