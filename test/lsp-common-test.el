43ea59d9cde9bab118ac8e7e22af96faa536d8bb
Adrian Ebeling <devl@adrian-ebeling.de>
2019-01-14 22:17:09 +0100

    adapt test case for downcasing paths on windows

--- test/lsp-common-test.el
+++ test/lsp-common-test.el
@@ -29,7 +29,7 @@
 (ert-deftest lsp--path-to-uri ()
   (let ((lsp--uri-file-prefix "file:///")
         (system-type 'windows-nt))
-    (should (equal (lsp--uri-to-path "file:///c:/Users/%7B%7D/") "c:/Users/{}/")))
+    (should (equal (lsp--uri-to-path "file:///c:/Users/%7B%7D/") "c:/users/{}/")))
   (let ((lsp--uri-file-prefix "file://"))
     (should (equal (lsp--uri-to-path "/root/%5E/%60") "/root/^/`"))))
 
