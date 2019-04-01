a9a5c87afc5f2de9d87cd144c20ee7d395d59a80
Vibhav Pant <vibhavp@gmail.com>
2019-04-01 18:49:41 +0530

    lsp-workspaces: Use cl-first instead of first.

--- lsp-mode.el
+++ lsp-mode.el
@@ -758,7 +758,7 @@ TRANSFORM-FN will be used to transform each of the items before displaying.
 PROMPT COLLECTION PREDICATE REQUIRE-MATCH INITIAL-INPUT HIST DEF
 INHERIT-INPUT-METHOD will be proxied to `completing-read' without changes."
   (let* ((result (--map (cons (funcall transform-fn it) it) collection))
-         (completion (completing-read prompt (-map 'first result)
+         (completion (completing-read prompt (-map 'cl-first result)
                                       predicate require-match initial-input hist
                                       def inherit-input-method)))
     (cdr (assoc completion result))))
