2ae2aaf317b7c68d51159c413341961aa11db11a
Ivan Yonchovski <yyoncho@gmail.com>
2019-04-13 14:24:58 +0300

    Setup markdown mode after enabling gfm-view-mode

--- lsp-mode.el
+++ lsp-mode.el
@@ -3163,18 +3163,18 @@ Stolen from `org-copy-visible'."
 
 (defun lsp--render-markdown ()
   "Render markdown."
-  (lsp--setup-markdown major-mode)
 
   (goto-char (point-min))
   (while (re-search-forward "&gt;" nil t)
     (replace-match ">"))
 
   (goto-char (point-min))
+
   (while (re-search-forward "&lt;" nil t)
     (replace-match "<"))
 
   (gfm-view-mode)
-  (font-lock-ensure))
+  (lsp--setup-markdown major-mode))
 
 (defun lsp--fontlock-with-mode (str mode)
   "Fontlock STR with MODE."
