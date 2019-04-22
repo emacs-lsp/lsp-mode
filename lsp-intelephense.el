5b7f178aea7eec96cb6771eddb95928daf0281d6
Norio Suzuki <norio.suzuki@gmail.com>
2019-04-22 18:52:50 +0900

    Modify to expand "~" to the absolute path

--- lsp-intelephense.el
+++ lsp-intelephense.el
@@ -82,7 +82,7 @@
   language server."
   :type '(choice (:tag "off" "messages" "verbose")))
 
-(defcustom lsp-intelephense-storage-path (locate-user-emacs-file "lsp-cache")
+(defcustom lsp-intelephense-storage-path (expand-file-name (locate-user-emacs-file "lsp-cache"))
   "Optional absolute path to storage dir."
   :group 'lsp-php-ip
   :type 'directory)
