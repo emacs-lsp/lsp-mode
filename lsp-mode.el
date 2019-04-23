4c87e811a1cd51564cfa8f09d6d7417076a79d6a
aureumleonis <xbullethammer@hotmail.com>
2019-04-22 22:35:06 -0700

    Fix symlink handling in project root recognition (#783)
    * Fix symlink handling in project root recognition
    
    * Updated lsp-workspace-root to use f-canonical for nil case

--- lsp-mode.el
+++ lsp-mode.el
@@ -2884,7 +2884,7 @@ Applies on type formatting."
   (when-let (file-name (or path (buffer-file-name)))
     (->> (lsp-session)
          (lsp-session-folders)
-         (--first (f-ancestor-of? it file-name)))))
+         (--first (f-ancestor-of? it (f-canonical file-name))))))
 
 (defun lsp-on-revert ()
   "Executed when a file is reverted.
@@ -4878,12 +4878,13 @@ Returns nil if the project should not be added to the current SESSION."
 
 (defun lsp-find-session-folder (session file-name)
   "Look in the current SESSION for folder containing FILE-NAME."
-  (->> session
-       (lsp-session-folders)
-       (--filter (or (f-same? it file-name)
-                     (f-ancestor-of? it file-name)))
-       (--max-by (> (length it)
-                    (length other)))))
+  (let ((file-name-canonical (f-canonical file-name)))
+    (->> session
+	 (lsp-session-folders)
+	 (--filter (or (f-same? it file-name-canonical)
+                       (f-ancestor-of? it file-name-canonical)))
+	 (--max-by (> (length it)
+                      (length other))))))
 
 (defun lsp-find-workspace (server-id file-name)
   "Find workspace for SERVER-ID for FILE-NAME."
