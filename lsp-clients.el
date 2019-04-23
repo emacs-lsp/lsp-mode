d2bc67710727303412612d38adc5ed4c69555e45
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2019-04-23 19:12:07 +1200

    Check file suffix only

--- lsp-clients.el
+++ lsp-clients.el
@@ -97,13 +97,12 @@ finding the executable with variable `exec-path'."
   :risky t
   :type '(repeat string))
 
-(defun lsp-typescript-javascript-tsx-jsx-activate-p (filename mode)
-  "Check if the javascript-typescript language server should be enabled
-based on FILE-NAME and MAJOR-MODE"
-  (or (member mode '(typescript-mode typescript-tsx-mode js-mode js-jsx-mode js2-mode js2-jsx-mode rjsx-mode))
-      (and (provided-mode-derived-p mode 'web-mode)
-           (or (string-suffix-p ".tsx" filename t)
-               (string-suffix-p ".jsx" filename t)))))
+(defun lsp-typescript-javascript-tsx-jsx-activate-p (filename _)
+  "Check if the javascript-typescript language server should be enabled based on FILENAME."
+  (or (string-suffix-p ".tsx" filename t)
+      (string-suffix-p ".jsx" filename t)
+      (string-suffix-p ".js" filename t)
+      (string-suffix-p ".ts" filename t)))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-stdio-connection (lambda ()
