3083066365db3782eb3c6d10ab992d9a0dd8bc37
dsyzling <dsyzling@gmail.com>
2019-05-15 16:40:39 +0100

    Add scala to lsp-language-id-configuration. (#836)

--- lsp-mode.el
+++ lsp-mode.el
@@ -509,6 +509,7 @@ If set to `:none' neither of two will be enabled."
 
 (defvar lsp-language-id-configuration '((".*.vue" . "vue")
                                         (".*.tsx" . "typescriptreact")
+                                        (scala-mode . "scala")
                                         (julia-mode . "julia")
                                         (java-mode . "java")
                                         (python-mode . "python")
