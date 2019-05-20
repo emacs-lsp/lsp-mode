26804854904a4c2030d20107c8b0c81c3a8929dc
Vibhav Pant <vibhavp@gmail.com>
2019-05-20 17:57:41 +0530

    Rename lsp-mode.html to index.html

--- travis/push_gh_pages.bash
+++ travis/push_gh_pages.bash
@@ -4,6 +4,7 @@ cd $1
 git init
 git config user.name "${GH_USER_NAME}"
 git config user.email "{GH_USER_EMAIL}"
+cp lsp-mode.html index.html
 git add .
 git commit -m "Deploy to GitHub Pages"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
