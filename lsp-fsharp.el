e4efbab6704e6b1241cccfa0992dbcc4ba08cdcb
Ivan Yonchovski <yyoncho@gmail.com>
2019-08-13 07:49:57 +0300

    Fix call to format in lsp-fsharp.el
    Fixes #970

--- lsp-fsharp.el
+++ lsp-fsharp.el
@@ -92,7 +92,7 @@ To use the mono/.Net framework version, set this to \"https://ci.appveyor.com/ap
          (install-dir-full (expand-file-name lsp-fsharp-server-install-dir))
          (unzip-script (cond ((executable-find "unzip") (format "mkdir -p %s && unzip -qq %s -d %s" install-dir-full temp-file install-dir-full))
                              ((executable-find "powershell") (format "powershell -noprofile -noninteractive -nologo -ex bypass Expand-Archive -path '%s' -dest '%s'" temp-file install-dir-full))
-                             (t (user-error (format "Unable to unzip server - file %s cannot be extracted, please extract it manually") temp-file)))))
+                             (t (user-error (format "Unable to unzip server - file %s cannot be extracted, please extract it manually" temp-file))))))
     (url-copy-file lsp-fsharp-server-download-url temp-file t)
     (shell-command unzip-script)
     (shell-command (format "%s %s --version" (lsp-fsharp--fsac-runtime-cmd) (lsp-fsharp--fsac-cmd)))))
