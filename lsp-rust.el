875fe19659fb49eb37935e295275bd0f38b16f41
Samir Hafez <me@samirhafez.com>
2019-10-10 07:37:35 +0100

    [Rust] Fix unactionable config (#1084)
    * Fix list of blacklisted crates
    
    * Remove the offending characters

--- lsp-rust.el
+++ lsp-rust.el
@@ -115,28 +115,25 @@ the latest build duration."
   :package-version '(lsp-mode . "6.1"))
 
 (defcustom lsp-rust-crate-blacklist  [
-                                      "cocoa",
-                                      "gleam",
-                                      "glium",
-                                      "idna",
-                                      "libc",
-                                      "openssl",
-                                      "rustc_serialize",
-                                      "serde",
-                                      "serde_json",
-                                      "typenum",
-                                      "unicode_normalization",
-                                      "unicode_segmentation",
+                                      "cocoa"
+                                      "gleam"
+                                      "glium"
+                                      "idna"
+                                      "libc"
+                                      "openssl"
+                                      "rustc_serialize"
+                                      "serde"
+                                      "serde_json"
+                                      "typenum"
+                                      "unicode_normalization"
+                                      "unicode_segmentation"
                                       "winapi"
                                       ]
   "A list of Cargo crates to blacklist."
   :type '(restricted-sexp :match-alternatives (lambda (xs)
                                                 (and
                                                  (vectorp xs)
-                                                 (seq-every-p #'stringp xs))))
-  :group 'lsp-rust
-  :package-version '(lsp-mode . "6.1"))
-
+                                                 (seq-every-p #'stringp xs)))))
 
 (defcustom lsp-rust-build-on-save nil
   "Only index the project when a file is saved and not on
