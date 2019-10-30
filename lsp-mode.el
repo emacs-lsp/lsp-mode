c4642d46b6252e9cca84b4208b6018c3af0e92b6
Sun JianKang <sjkyspa@gmail.com>
2019-10-31 00:13:15 +0800

    fix: make-progress-reporter exception caused by nil log--info (#1130)
    * fix: make-progress-reporter exception caused by nil log--info
    
    Debugger entered--Lisp error: (wrong-type-argument stringp nil)
      string-match("[[:alnum:]]\\'" nil)
      make-progress-reporter(nil 0 1)
      lsp--apply-text-edits((#<hash-table equal 2/65 0x41ec94a9>))
      lsp--apply-formatting((#<hash-table equal 2/65 0x41ec94a9>))
    
    * fix: try to fix empty progress reporter error with empty lsp-info

--- lsp-mode.el
+++ lsp-mode.el
@@ -3109,8 +3109,8 @@ This method is used if we do not have `buffer-replace-content'."
                              (prepare-change-group)))
              (howmany (length edits))
              (reporter (make-progress-reporter
-                        (lsp--info "Applying %s edits to `%s'..."
-                                   howmany (current-buffer))
+                        (or (lsp--info "Applying %s edits to `%s' ..."
+                                   howmany (current-buffer)) "Applying edits ...")
                         0 howmany))
              (done 0)
              (apply-edit (if (functionp 'replace-buffer-contents)
