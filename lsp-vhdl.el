1e6838cf48c090647cf772f538294dca610565c3
Christian Birk Sørensen <chrbirks@gmail.com>
2019-11-12 20:45:38 +0100

    Added support for HDL Checker server to lsp-vhdl

--- lsp-vhdl.el
+++ lsp-vhdl.el
@@ -21,44 +21,84 @@
 
 ;;; Commentary:
 
-;; LSP support for VHDL using the language server from http://www.vhdltool.com/download.
-;; Set the following symbol to specify the path of the language server:
+;; LSP support for VHDL using using an external language server. Currently
+;; the supported servers are:
 ;;
-;; (setq lsp-vhdl-server-path "/path/to/server_binary")
-;;
+;; VHDL-tool from http://www.vhdltool.com/download
 ;; The server requires a project file list called vhdltool-config.yaml which must
 ;; be placed the the project root. An example file can be found here:
 ;; http://www.vhdltool.com/configuration
+;;
+;; HDL Checker from https://github.com/suoto/hdl_checker.
+;; This server requires a config file described here
+;; https://github.com/suoto/hdl_checker/wiki/setting-up-a-project
+;;
+;; Set the symbol lsp-vhdl-server to select the language server and set
+;; lsp-vhdl-server-path if the binary is not in the user PATH.
 
 ;;; Code:
 
 (require 'lsp-mode)
 
-(defgroup lsp-vhdl nil
-  "LSP support for VHDL using the server from http://www.vhdltool.com"
-  :group 'lsp-mode
-  :link '(url-link "http://www.vhdltool.com"))
+(defvar vhdl-tool-bin-name "vhdl-tool"
+  "Name of the VHDL Tool binary.")
 
-(defcustom lsp-vhdl-server-path
-  "NOT_SET"
-  "Path to binary server file downloaded from http://www.vhdltool.com/download"
+(defvar vhdl-tool-disp-name "lsp-vhdl-tool"
+  "Display name for VHDL-tool.")
+
+(defvar hdl-checker-bin-name "hdl_checker"
+  "Name of HDL Checker binary.")
+
+(defvar hdl-checker-disp-name "lsp-hdl-checker"
+  "Display name for HDL Checker.")
+
+(defgroup lsp-vhdl nil
+  "LSP support for VHDL. Set lsp-vhdl-server to select server. The default is to use VHDL-tool."
+  :group 'lsp-mode)
+
+(defcustom lsp-vhdl-server 'vhdl-tool
+  "Select which server to use:
+VHDL-tool: A syntax checking, type checking and linting tool (http://vhdltool.com).
+HDL Checker: A wrapper for third party tools such as GHDL, ModelSim, Vivado Simulator (https://github.com/suoto/hdl_checker)."
+  :type '(choice (const :tag "VHDL-tool" vhdl-tool)
+                 (const :tag "HDL Checker" hdl-checker))
+  :group 'lsp-vhdl)
+
+(defcustom lsp-vhdl-server-path nil
+  "Path to binary server file."
   :group 'lsp-vhdl
   :risky t
   :type 'file)
 
 (defun lsp-vhdl--create-connection ()
-  "Returns lsp-stdio-connection or an error if server not found"
-  (lsp-stdio-connection
-   (lambda ()
-     (list lsp-vhdl-server-path "lsp"))))
+  "Returns lsp-stdio-connection based on the selected server"
+  (setq params nil)
+  (lsp-vhdl--set-server-path)
+  (lsp-vhdl--set-server-args)
+  (plist-put
+   (lsp-stdio-connection (lambda () (list (plist-get params 'server-path) (plist-get params 'server-args))))
+   :test? (lambda () (f-executable? (plist-get params 'server-path)))))
+
+(defun lsp-vhdl--set-server-path()
+  "Set path to server binary based on selection in lsp-vhdl-server."
+  (cond ((eq lsp-vhdl-server 'hdl-checker) (if (eq lsp-vhdl-server-path nil)
+                                               (setq params (plist-put params 'server-path hdl-checker-bin-name))
+                                             (setq params (plist-put params 'server-path lsp-vhdl-server-path))))
+        ((eq lsp-vhdl-server 'vhdl-tool) (if (eq lsp-vhdl-server-path nil)
+                                             (setq params (plist-put params 'server-path vhdl-tool-bin-name))
+                                           (setq params (plist-put params 'server-path lsp-vhdl-server-path))))))
+
+(defun lsp-vhdl--set-server-args()
+  "Set server arguments based on server selection."
+  (cond ((eq lsp-vhdl-server 'hdl-checker) (setq params (plist-put params 'server-args "--lsp")))
+        ((eq lsp-vhdl-server 'vhdl-tool) (setq params (plist-put params 'server-args "lsp")))))
 
 (lsp-register-client
  (make-lsp-client :new-connection (lsp-vhdl--create-connection)
                   :major-modes '(vhdl-mode)
                   :language-id "VHDL"
 		              :priority -1
-                  :server-id 'lsp-vhdl-server)
- )
+                  :server-id 'lsp-vhdl))
 
 (provide 'lsp-vhdl)
 ;;; lsp-vhdl.el ends here
