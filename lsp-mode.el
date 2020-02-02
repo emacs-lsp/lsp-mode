04ea837bd1b0d70ded372fb525a31cb3728e1e91
Eric Berquist <eric.berquist@gmail.com>
2020-02-02 18:20:37 -0500

    Add CMake language server (#1372)
    Adds https://github.com/regen100/cmake-language-server.

--- lsp-mode.el
+++ lsp-mode.el
@@ -633,7 +633,8 @@ Changes take effect only when a new session is started."
                                         (ess-r-mode . "r")
                                         (crystal-mode . "crystal")
                                         (nim-mode . "nim")
-                                        (dhall-mode . "dhall"))
+                                        (dhall-mode . "dhall")
+                                        (cmake-mode . "cmake"))
   "Language id configuration.")
 
 (defvar lsp--last-active-workspaces nil
