d33dcd891526e7ee3368b42aec6e6e6c77f74c4a
Reza Nikoopour <rnikoopour@gmail.com>
2020-02-14 20:08:56 -0800

    Fixed tests

--- test/lsp-clients-test.el
+++ test/lsp-clients-test.el
@@ -76,13 +76,15 @@
     (should (not (lsp-clients-flow-activate-p (concat test-location "fixtures/SampleTypeScriptProject/src/sample.ts") nil)))))
 
 (ert-deftest lsp-typescript-javascript-activates-based-on-file-extension ()
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "abc.js"))
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "abc.jsx"))
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "abc.ts"))
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "abc.tsx"))
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "a1.ts"))
-  (should (lsp-typescript-javascript-tsx-jsx-activate-p "a1.d.ts"))
-  (should (not (lsp-typescript-javascript-tsx-jsx-activate-p "abc.tsxx")))
-  (should (not (lsp-typescript-javascript-tsx-jsx-activate-p "abc.jss"))))
+  (should (lsp-typescript-javascript-jsx-activate-p "abc.js"))
+  (should (lsp-typescript-javascript-jsx-activate-p "abc.jsx"))
+  (should (lsp-typescript-javascript-tsx-activate-p "abc.ts"))
+  (should (lsp-typescript-javascript-tsx-activate-p "abc.tsx"))
+  (should (lsp-typescript-javascript-tsx-activate-p "a1.ts"))
+  (should (lsp-typescript-javascript-tsx-activate-p "a1.d.ts"))
+  (should (not (and (lsp-typescript-javascript-jsx-activate-p "abc.tsxx")
+		    (lsp-typescript-javascript-tsx-activate-p "abc.tsxx"))))
+  (should (not (and (lsp-typescript-javascript-jsx-activate-p "abc.jss")
+		    (lsp-typescript-javascript-tsx-activate-p "abc.jss")))))
 
 ;;; lsp-clients-test.el ends here
