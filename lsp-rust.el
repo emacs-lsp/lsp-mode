9a10f8f044b723ca5d59b397f96365eeca9d4daf
brotzeit <brotzeitmacher@gmail.com>
2020-02-23 10:29:11 +0100

    update rust-analyzer binary
    close #1431

--- lsp-rust.el
+++ lsp-rust.el
@@ -318,7 +318,7 @@ PARAMS progress report notification data."
 
 ;; rust-analyzer
 
-(defcustom lsp-rust-analyzer-server-command '("ra_lsp_server")
+(defcustom lsp-rust-analyzer-server-command '("~/.cargo/bin/rust-analyzer")
   "Command to start rust-analyzer."
   :type '(repeat string)
   :package-version '(lsp-mode . "6.2"))
