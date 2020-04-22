d89f403fbf629f4c6b95d63115a4959547cea531
Alan Zimmerman <alan.zimm@gmail.com>
2020-04-22 20:34:02 +0100

    Use when-let to simplify progress reporter code (#1603)
    As mentioned in https://github.com/emacs-lsp/lsp-mode/pull/1602#pullrequestreview-398270661

--- lsp-mode.el
+++ lsp-mode.el
@@ -1529,15 +1529,13 @@ WORKSPACE is the workspace that contains the progress token."
           (lsp-workspace-set-work-done-token token reporter workspace)))
 
        ("report"
-        (let ((reporter (lsp-workspace-get-work-done-token token workspace)))
-          (when reporter
-            (progress-reporter-update reporter (gethash "percentage" value nil)))))
+        (when-let ((reporter (lsp-workspace-get-work-done-token token workspace)))
+            (progress-reporter-update reporter (gethash "percentage" value nil))))
 
        ("end"
-        (let ((reporter (lsp-workspace-get-work-done-token token workspace)))
-          (when reporter
-            (progn (progress-reporter-done reporter)
-                   (lsp-workspace-rem-work-done-token token workspace))))))))
+        (when-let ((reporter (lsp-workspace-get-work-done-token token workspace)))
+            (progress-reporter-done reporter)
+            (lsp-workspace-rem-work-done-token token workspace))))))
 
 (defun lsp-diagnostics (&optional current-workspace?)
   "Return the diagnostics from all workspaces."
