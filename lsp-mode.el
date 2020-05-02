bdbd2d0ccfe02f6721ea942e82e5178f4b216228
Ivan Yonchovski <yyoncho@gmail.com>
2020-05-02 07:53:36 +0300

    Bump version to 6.3.1
    - see https://github.com/emacs-lsp/lsp-mode/issues/1636

--- lsp-mode.el
+++ lsp-mode.el
@@ -1,11 +1,11 @@
 ;;; lsp-mode.el --- LSP mode                              -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  Vibhav Pant, Ivan Yonchovski
+;; Copyright (C) 2020 emacs-lsp maintainers
 
 ;; Author: Vibhav Pant, Fangrui Song, Ivan Yonchovski
 ;; Keywords: languages
 ;; Package-Requires: ((emacs "25.1") (dash "2.14.1") (dash-functional "2.14.1") (f "0.20.0") (ht "2.0") (spinner "1.7.3") (markdown-mode "2.3") (lv "0"))
-;; Version: 6.3
+;; Version: 6.3.1
 
 ;; URL: https://github.com/emacs-lsp/lsp-mode
 ;; This program is free software; you can redistribute it and/or modify
