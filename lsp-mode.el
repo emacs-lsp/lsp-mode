d2f55fa37e3b22dbc6abf5357a9cef87772e8780
sasanidas <fmfs@posteo.net>
2020-05-10 17:19:49 +0200

    Add serenata to lsp-mode file

--- lsp-mode.el
+++ lsp-mode.el
@@ -215,7 +215,7 @@ the buffer when it becomes large."
   '(ccls cquery lsp-clients lsp-clojure lsp-csharp lsp-css lsp-dart lsp-elm
     lsp-erlang lsp-eslint lsp-fsharp lsp-gdscript lsp-go lsp-haskell lsp-haxe
     lsp-intelephense lsp-java lsp-json lsp-metals lsp-perl lsp-pwsh lsp-pyls
-    lsp-python-ms lsp-rust lsp-solargraph lsp-terraform lsp-verilog lsp-vetur
+    lsp-python-ms lsp-rust lsp-serenata lsp-solargraph lsp-terraform lsp-verilog lsp-vetur
     lsp-vhdl lsp-xml lsp-yaml)
   "List of the clients to be automatically required."
   :group 'lsp-mode
