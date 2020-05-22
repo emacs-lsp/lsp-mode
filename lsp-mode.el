809c6488ecfd584dce458b067bf613db85cbe07e
Brian Leung <29217594+leungbk@users.noreply.github.com>
2020-05-21 22:06:31 -0700

    Filter out non-ranges where the beginning is greater than or equal... (#1712)
    * Minor simplifications
    
    nconc is preferred over append since append makes a copy of all lists
    preceding the final argument.
    
    let* isn't necessary.
    
    * Avoid repeated invocations of lsp--line-character-to-point...
    
    ...when computing folding ranges.
    
    * Filter out non-ranges where the beginning is greater than or equal...
    
    ...to the end of the folding range.

--- lsp-mode.el
+++ lsp-mode.el
@@ -1781,31 +1781,50 @@ WORKSPACE is the workspace that contains the diagnostics."
 
 (defun lsp--get-folding-ranges ()
   "Get the folding ranges for the current buffer."
-  (-let [(tick . ranges) lsp--cached-folding-ranges]
-    (if (eq tick (buffer-chars-modified-tick))
-        ranges
-      (setq ranges (lsp-request "textDocument/foldingRange"
+  (unless (eq (buffer-chars-modified-tick) (car lsp--cached-folding-ranges))
+    (let* ((ranges (lsp-request "textDocument/foldingRange"
                                 `(:textDocument ,(lsp--text-document-identifier))))
+           (sorted-line-col-pairs (-sort
+                                   #'lsp--line-col-comparator
+                                   (apply
+                                    #'nconc
+                                    (seq-map
+                                     (lambda (range)
+                                       (-let [(&hash "startLine" start-line
+                                                     "startCharacter" start-character
+                                                     "endLine" end-line
+                                                     "endCharacter" end-character)
+                                              range]
+                                         (list (cons start-line start-character)
+                                               (cons end-line end-character))))
+                                     ranges))))
+           (line-col-to-point-map (lsp--convert-line-col-to-points-batch
+                                   sorted-line-col-pairs)))
       (setq lsp--cached-folding-ranges
             (cons (buffer-chars-modified-tick)
-                  (-> (lambda (range)
-                        (-let [(&hash "startLine" start-line
-                                      "startCharacter" start-character
-                                      "endLine" end-line
-                                      "endCharacter" end-character
-                                      "kind" kind)
-                               range]
-                          (make-lsp--folding-range
-                           :beg (lsp--line-character-to-point
-                                 start-line start-character)
-                           :end (lsp--line-character-to-point
-                                 end-line end-character)
-                           :kind kind
-                           :orig-folding-range range)))
-                      (seq-map ranges)
-                      (seq-into 'list)
-                      (delete-dups))))
-      (cdr lsp--cached-folding-ranges))))
+                  (seq-filter (lambda (folding-range)
+                                (< (lsp--folding-range-beg folding-range)
+                                   (lsp--folding-range-end folding-range)))
+                              (delete-dups
+                               (seq-into
+                                (seq-map
+                                 (lambda (range)
+                                   (-let [(&hash "startLine" start-line
+                                                 "startCharacter" start-character
+                                                 "endLine" end-line
+                                                 "endCharacter" end-character
+                                                 "kind" kind)
+                                          range]
+                                     (make-lsp--folding-range
+                                      :beg (ht-get line-col-to-point-map
+                                                   (cons start-line start-character))
+                                      :end (ht-get line-col-to-point-map
+                                                   (cons end-line end-character))
+                                      :kind kind
+                                      :orig-folding-range range)))
+                                 ranges)
+                                'list)))))))
+  (cdr lsp--cached-folding-ranges))
 
 (defun lsp--get-nested-folding-ranges ()
   "Get a list of nested folding ranges for the current buffer."
@@ -6336,15 +6355,15 @@ an alist
       (let ((sym (pop stack)))
         (push (lsp--get-line-and-col sym) line-col-list)
         (unless (seq-empty-p (gethash "children" sym))
-          (setf stack (append (lsp--imenu-filter-symbols (gethash "children" sym)) stack)))))
+          (setf stack (nconc (lsp--imenu-filter-symbols (gethash "children" sym)) stack)))))
     (-sort #'lsp--line-col-comparator line-col-list)))
 
 (defun lsp--convert-line-col-to-points-batch (line-col-list)
   "Convert a sorted list of positions from line-column
 representation to point representation."
-  (let* ((line-col-to-point-map (ht-create))
-         (inhibit-field-text-motion t)
-         (curr-line 0))
+  (let ((line-col-to-point-map (ht-create))
+        (inhibit-field-text-motion t)
+        (curr-line 0))
     (save-excursion
       (save-restriction
         (widen)
@@ -6363,7 +6382,11 @@ representation to point representation."
 
 (cl-defun lsp--line-col-comparator ((l1 . c1) (l2 . c2))
   (or (< l1 l2)
-      (and (= l1 l2) (< c1 c2))))
+      (and (= l1 l2)
+           (cond ((and c1 c2)
+                  (< c1 c2))
+                 (c1 t)
+                 (c2 nil)))))
 
 (defun lsp--imenu-create-index ()
   "Create imenu index from document symbols."
