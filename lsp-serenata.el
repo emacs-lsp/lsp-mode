fa63033bd05fba6e89930dfffb64a03abb1d560b
Sasanidas <fmfs@posteo.net>
2020-07-02 19:43:51 +0200

    Add lsp-serenata notification handler and init options (#1864)
    * Add serenata notification handler and init options
    
    * Unnecesary line
    
    * Add Serenata progressIndexing interface
    
    * Fix CI errors
    
    * Add interface data types
    
    * Update lsp-serenata.el
    
    * Update lsp-serenata.el
    
    Co-authored-by: kiennq <kien.n.quang@gmail.com>
    
    Co-authored-by: fermin <fermin@Fdebian>
    Co-authored-by: Ivan Yonchovski <yyoncho@users.noreply.github.com>
    Co-authored-by: kiennq <kien.n.quang@gmail.com>

--- lsp-serenata.el
+++ lsp-serenata.el
@@ -24,6 +24,7 @@
 
 ;;; Code:
 
+(require 'lsp-protocol)
 (require 'lsp-mode)
 
 (defgroup lsp-serenata nil
@@ -39,21 +40,58 @@ It can be downloaded from https://gitlab.com/Serenata/Serenata/-/releases."
   :group 'lsp-serenata
   :type 'file)
 
+(defcustom lsp-serenata-php-version
+  7.3
+  "Allows you to specify the PHP version your project is written in.
+At the moment this directive is still ignored, but it will
+influence functionality such as refactoring in the future, where
+older PHP versions may not support scalar type hints, which may
+then be omitted from places such as getters and setters."
+  :group 'lsp-serenata
+  :type 'number)
+
+(defcustom lsp-serenata-file-extensions
+  ["php"]
+  "List of file extensions (without dot) to process.
+Files that do not match this whitelist will be ignored during
+indexing.  Usually you'll want to set this to at least include
+php, as it is the most common PHP extension.  phpt is not
+included by default as it is often used to contain test code that
+is not directly part of the code.  Note that for existing
+projects, removing extensions will not not automatically prune
+files having them from the index if they are already present.
+Adding new ones will cause the files having them to be picked up
+on the next project initialization."
+  :group 'lsp-serenata
+  :type 'lsp-string-vector)
+
 (defun lsp-serenata-server-start-fun (port)
   "Define serenata start function, it requires a PORT."
   `(,lsp-serenata-server-path
     "-u" ,(number-to-string port)))
+
+(defun lsp-serenata-init-options ()
+  "Init options for lsp-serenata."
+  `(
+    :phpVersion ,lsp-serenata-php-version
+    :fileExtensions ,lsp-serenata-file-extensions))
+
+(lsp-interface (serenata:didProgressIndexing (:sequenceOfIndexedItem :totalItemsToIndex :progressPercentage :folderUri :fileUri :info) nil ))
 
 (lsp-register-client
  (make-lsp-client
   :new-connection (lsp-tcp-connection 'lsp-serenata-server-start-fun)
   :major-modes '(php-mode)
   :priority -2
-  :server-id 'serenata
+  :notification-handlers (ht ("serenata/didProgressIndexing"
+			      (lambda (_server data)
+				(lsp--info "%s" (lsp:serenata-did-progress-indexing-info data)) )))
+  :initialization-options #'lsp-serenata-init-options
   :initialized-fn (lambda (workspace)
                     (with-lsp-workspace workspace
                       (lsp--set-configuration
-                       (lsp-configuration-section "serenata"))))))
+                       (lsp-configuration-section "serenata"))))
+  :server-id 'serenata))
 
 (provide 'lsp-serenata)
 ;;; lsp-serenata.el ends here
