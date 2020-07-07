58cc7efda0c74ca91e3d3e1693742b40a4f61249
kiennq <kien.n.quang@gmail.com>
2020-07-07 14:17:54 +0900

    Revert "lsp--capf-cache: also clear cache on company completion start (#1887)"
    This reverts commit b7f40dc6e57a11d30c0ee560eb97ab29daa50565.

--- lsp-mode.el
+++ lsp-mode.el
@@ -7146,7 +7146,6 @@ returns the command to execute."
 (defun lsp--setup-company ()
   (add-hook 'company-completion-started-hook
             (lambda (&rest _)
-              (lsp--capf-clear-cache)
               (setq-local lsp-inhibit-lsp-hooks t))
             nil
             t)
