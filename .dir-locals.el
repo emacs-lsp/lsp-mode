90382b8b9f65ba98c64c33a9b90f162db7acd32e
Brian Leung <leungbk@mailfence.com>
2020-07-16 13:33:09 -0700

    Add lsp-defun to imenu-generic-expression for emacs-lisp-mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,15 @@
 ((nil
   (require-final-newline . t))
  (emacs-lisp-mode
+  (eval . (let ((dirloc-lsp-defun-regexp
+                 (concat
+                  (concat "^\\s-*("
+                          "lsp-defun"
+                          "\\s-+\\(")
+                  (or (bound-and-true-p lisp-mode-symbol-regexp)
+                      "\\(?:\\sw\\|\\s_\\|\\\\.\\)+")
+                  "\\)")))
+            (add-to-list 'imenu-generic-expression
+                         (list "Functions" dirloc-lsp-defun-regexp 1))))
   (flycheck-disabled-checkers . '(emacs-lisp-checkdoc))
   (indent-tabs-mode . nil)))
