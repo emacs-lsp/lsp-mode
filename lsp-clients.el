720ec4f3334b4fba61db0cdc331fb9630095a66e
E. Alexander Barbosa <40282381+elxbarbosa@users.noreply.github.com>
2020-08-12 18:11:50 -0300

    Ada client to lsp-ada.el file (#2050)
    * Ada client to lsp-ada.el file
    
    * keep old variables name.
    
    * Remove comment
    
    Co-authored-by: Eric Dallo <ercdll1337@gmail.com>

--- lsp-clients.el
+++ lsp-clients.el
@@ -30,50 +30,6 @@
 (require 'rx)
 (require 'cl-lib)
 
-;;; Ada
-(defgroup lsp-ada nil
-  "Settings for Ada Language Server."
-  :group 'tools
-  :tag "Language Server"
-  :package-version '(lsp-mode . "6.2"))
-
-(defcustom lsp-ada-project-file "default.gpr"
-  "Set the project file full path to configure the language server with.
-  The ~ prefix (for the user home directory) is supported.
-  See https://github.com/AdaCore/ada_language_server for a per-project
-  configuration example."
-  :type 'string
-  :group 'lsp-ada
-  :package-version '(lsp-mode . "6.2"))
-
-(defcustom lsp-ada-option-charset "UTF-8"
-  "The charset to use by the Ada Language server. Defaults to 'UTF-8'."
-  :type 'string
-  :group 'lsp-ada
-  :package-version '(lsp-mode . "6.2"))
-
-(defcustom lsp-ada-enable-diagnostics t
-  "A boolean to disable diagnostics. Defaults to true."
-  :type 'boolean
-  :group 'lsp-ada
-  :package-version '(lsp-mode . "6.2"))
-
-(lsp-register-custom-settings
- '(("ada.projectFile" lsp-ada-project-file)
-   ("ada.enableDiagnostics" lsp-ada-enable-diagnostics)
-   ("ada.defaultCharset" lsp-ada-option-charset)))
-
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection '("ada_language_server"))
-                  :major-modes '(ada-mode)
-                  :priority -1
-                  :initialized-fn (lambda (workspace)
-                                    (with-lsp-workspace workspace
-                                      (lsp--set-configuration
-                                       (lsp-configuration-section "ada"))))
-                  :server-id 'ada-ls))
-
-
 
 ;;; Groovy
 (defgroup lsp-groovy nil
