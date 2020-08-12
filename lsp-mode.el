7cf96276569efd25618d09480c03f916bb617b1a
elxbarbosa <elxbarbosa@outlook.com>
2020-08-12 00:25:42 -0300

    JavaScript and TypeScript clients to lsp-javascript.el

--- lsp-mode.el
+++ lsp-mode.el
@@ -350,7 +350,7 @@ unless overridden by a more specific face association."
 (defcustom lsp-client-packages
   '(ccls lsp-clients lsp-clojure lsp-csharp lsp-css lsp-dart lsp-elm
          lsp-erlang lsp-eslint lsp-fsharp lsp-gdscript lsp-go lsp-haskell lsp-haxe
-         lsp-intelephense lsp-java lsp-json lsp-lua lsp-metals lsp-perl lsp-pwsh lsp-pyls
+         lsp-intelephense lsp-java lsp-javascript lsp-json lsp-lua lsp-metals lsp-perl lsp-pwsh lsp-pyls
          lsp-python-ms lsp-rust lsp-serenata lsp-solargraph lsp-terraform lsp-verilog lsp-vetur
          lsp-vhdl lsp-xml lsp-yaml lsp-sqls lsp-svelte)
   "List of the clients to be automatically required."
