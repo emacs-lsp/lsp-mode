fbf5d171eae143c0a6e629df74c31c70dfefb64f
E. Alexander Barbosa <40282381+elxbarbosa@users.noreply.github.com>
2020-08-16 10:35:06 -0300

    Nim clients to lsp-nim.el (#2057)
    * Nim clients to lsp-nim.el
    
    * remove left crystal references

--- lsp-mode.el
+++ lsp-mode.el
@@ -350,7 +350,7 @@ unless overridden by a more specific face association."
 (defcustom lsp-client-packages
   '(ccls lsp-ada lsp-angular lsp-bash lsp-clients lsp-clojure lsp-cmake lsp-crystal lsp-csharp lsp-css lsp-dart lsp-dhall
          lsp-dockerfile lsp-elm lsp-erlang lsp-eslint lsp-fsharp lsp-gdscript lsp-go lsp-haskell lsp-haxe
-         lsp-intelephense lsp-java lsp-json lsp-lua lsp-metals lsp-perl lsp-pwsh lsp-pyls
+         lsp-intelephense lsp-java lsp-json lsp-lua lsp-nim lsp-metals lsp-perl lsp-pwsh lsp-pyls
          lsp-python-ms lsp-r lsp-rf lsp-rust lsp-serenata lsp-solargraph lsp-terraform lsp-verilog lsp-vetur
          lsp-vhdl lsp-vimscript lsp-xml lsp-yaml lsp-sqls lsp-svelte)
   "List of the clients to be automatically required."
