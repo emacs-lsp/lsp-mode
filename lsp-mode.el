9efe3246ff688893ddbc4a2e76f021c87367f266
Ivan Yonchovski <yyoncho@gmail.com>
2020-09-03 08:42:13 +0300

    Map cython-mode to python language
    Fixes #2137

--- lsp-mode.el
+++ lsp-mode.el
@@ -813,6 +813,7 @@ Changes take effect only when a new session is started."
                                         (java-mode . "java")
                                         (groovy-mode . "groovy")
                                         (python-mode . "python")
+                                        (cython-mode . "python")
                                         (lsp--render-markdown . "markdown")
                                         (rust-mode . "rust")
                                         (rustic-mode . "rust")
