ebe3b70ba3bd2d9a86cc19053d761da4a7484161
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2020-09-10 05:03:46 +0100

    fix quoting

--- lsp-mode.el
+++ lsp-mode.el
@@ -1527,11 +1527,11 @@ FIND-COMMAND is the executable finder that searches for the
 actual language server executable. ARGS is a list of arguments to
 give to FIND-COMMAND to find the language server.
 
-Typical uses includes finding an executable by invoking `find' in
-a project, finding LLVM commands on macOS with `xcrun', or
+Typical uses includes finding an executable by invoking 'find' in
+a project, finding LLVM commands on macOS with 'xcrun', or
 looking up project-specific language servers for projects written
-in the various dynamic languages, e.g. `nvm', `pyenv' and
-`rbenv' etc."
+in the various dynamic languages, e.g. 'nvm', 'pyenv' and 'rbenv'
+etc."
   (when-let* ((find-command-path (executable-find find-command))
               (executable-path
                (with-temp-buffer
