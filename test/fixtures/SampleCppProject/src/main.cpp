8d580b785811c579d10f486ea04986edfcb8c472
petr-tik <petr-tik@users.noreply.github.com>
2020-10-27 23:01:15 +0000

    Add sample cpp project with compile_commands.json
    cmake-based project. Needs mkdir build && cmake .. to initialise

---
+++ test/fixtures/SampleCppProject/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "main.h"
+
+using namespace std;
+
+int multiply_by_seventeen(int param) {
+    return param * 17;
+}
+
+int main(int argc, char *argv[]) {
+    cout << "Wagwan world!" << "\n";
+
+    return 0;
+}
---
+++ test/fixtures/SampleCppProject/src/main.h
@@ -0,0 +1,1 @@
+int multiply_by_seventeen(int param);
