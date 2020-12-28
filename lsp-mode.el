92911b62523583f674a78d93478631bfb3ff0869
Nikita Bloshchanevich <nikblos@outlook.com>
2020-12-28 22:57:02 +0100

    Add an `iedit' command to edit documentHighlights
    Add a new command, `lsp-iedit-highlights', which will invoke `iedit' on
    the documentHighlights at point. It can be used as a primitive
    `lsp-rename' replacement if the former is unsupported.
    
    Implement it on top the new function, `lsp-iedit--on-ranges', which
    invokes `iedit' on a list of `&Range's. This separation will allow
    implementing a similar `iedit' commmand for `LinkedEditingRanges'.

--- lsp-mode.el
+++ lsp-mode.el
@@ -8385,6 +8385,43 @@ This avoids overloading the server with many files when starting Emacs."
     (lsp--error "Nothing to disconnect from?")))
 
 
+;;; `lsp-iedit'
+(lsp-defun lsp--range-text ((&RangeToPoint :start :end))
+  (buffer-substring start end))
+
+(defun lsp-iedit--on-ranges (ranges)
+  "Start an `iedit' operation using RANGES.
+RANGES shall be a list of lsp-`&Range's. They can be acquired
+from various lsp protocol requests, e.g.
+`textDocument/documentHighlight', ...."
+  (unless (seq-empty-p ranges)
+    (mapc (-lambda ((&RangeToPoint :start :end))
+            (push (iedit-make-occurrence-overlay start end)
+                  iedit-occurrences-overlays))
+          ranges)
+    ;; See `iedit-start'; TODO: upstream this
+    (setq iedit-mode t)
+    (when iedit-auto-buffering
+      (iedit-start-buffering))
+    (run-hooks 'iedit-mode-hook)
+    (add-hook 'before-revert-hook 'iedit-done nil t)
+    (add-hook 'kbd-macro-termination-hook 'iedit-done nil t)
+    (add-hook 'change-major-mode-hook 'iedit-done nil t)
+    (add-hook 'iedit-aborting-hook 'iedit-done nil t)
+    (message "%d occurrences of \"%s\""
+             (seq-length ranges)
+             (lsp--range-text (lsp-seq-first ranges)))))
+
+(defun lsp-iedit-highlights ()
+  "Start an `iedit' operation on the documentHighlights at point.
+This can be used as a primitive `lsp-rename' replacement if the
+language server doesn't support renaming.
+
+See also `lsp-enable-symbol-highlighting'."
+  (interactive)
+  (let ((highlights (lsp-request "textDocument/documentHighlight"
+                                 (lsp--text-document-position-params))))
+    (lsp-iedit--on-ranges (mapcar #'lsp:document-highlight-range highlights))))
 
 (provide 'lsp-mode)
 ;;; lsp-mode.el ends here
