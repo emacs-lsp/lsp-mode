39196cf3426c8569bb228301919fbf23e24be389
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2021-01-10 09:11:16 +0000

    js2-mode derives from js-mode

--- clients/lsp-javascript.el
+++ clients/lsp-javascript.el
@@ -45,7 +45,7 @@
 (defun lsp-typescript-javascript-tsx-jsx-activate-p (filename &optional _)
   "Check if the javascript-typescript language server should be enabled based on FILENAME."
   (or (string-match-p "\\`mjs\\|[jt]sx?\\'" (or (file-name-extension filename) ""))
-      (and (derived-mode-p 'js-mode 'js2-mode 'typescript-mode)
+      (and (derived-mode-p 'js-mode 'typescript-mode)
            (not (derived-mode-p 'json-mode)))))
 
 (lsp-register-client
