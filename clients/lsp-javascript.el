ebd71c8d405d2333a761b5de84661cca85ee97b2
Ivan Yonchovski <yyoncho@gmail.com>
2021-01-20 09:17:13 +0200

    Make sure ts-ls does not run in ejs files
    Fixes #2544

--- clients/lsp-javascript.el
+++ clients/lsp-javascript.el
@@ -44,7 +44,7 @@
 
 (defun lsp-typescript-javascript-tsx-jsx-activate-p (filename &optional _)
   "Check if the javascript-typescript language server should be enabled based on FILENAME."
-  (or (string-match-p "\\.mjs\\|[jt]sx?\\'" filename)
+  (or (string-match-p "\\.mjs\\|\\.[jt]sx?\\'" filename)
       (and (derived-mode-p 'js-mode 'typescript-mode)
            (not (derived-mode-p 'json-mode)))))
 
