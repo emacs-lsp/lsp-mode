4b61d206bd120251e6b5fb0b603d41efbacddef7
JenChieh <jcs090218@gmail.com>
2021-03-06 20:02:25 +0800

    Add test def

--- lsp.el
+++ lsp.el
@@ -3,6 +3,9 @@
 
 ;; (warn "Replace (require 'lsp) with (require 'lsp-mode)")
 
+;; TODO: Remove this, this is test if we install it from source.
+(defun lsp-test-packages () (message "[INFO ]Remove this after test."))
+
 (provide 'lsp)
 
 ;;; lsp.el ends here
