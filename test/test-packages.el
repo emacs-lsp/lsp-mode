d561733ef4ee9ff2b3c381f78cd777655e9e9ccc
Jen-Chieh Shen <jcs090218@gmail.com>
2021-03-30 21:46:04 +0800

    Fix CI (#2755)
    * Remove confirmation, wrong implementation
    
    * Try remove next package
    
    * Revert and remove celpa

--- test/test-packages.el
+++ test/test-packages.el
@@ -55,9 +55,7 @@
   (version<= (package-emacs-version pkg) emacs-version))
 
 
-(let* ((confirm-kill-processes nil)  ; don't ask to kill process
-       (package-archives '(("melpa" . "https://melpa.org/packages/")
-                           ("celpa" . "https://celpa.conao3.com/packages/")
+(let* ((package-archives '(("melpa" . "https://melpa.org/packages/")
                            ("gnu" . "https://elpa.gnu.org/packages/")))
        (pkgs '(ccls
                dap-mode
