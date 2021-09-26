39172d55d7372eff915caa769902c35242f294ed
Ivan Yonchovski <yyoncho@gmail.com>
2021-09-26 13:28:41 +0300

    Allow configuring lsp-use-plists using command line
    - Without this it is hard to configure emacs to use `lsp-use-plists` = t when
    using native compilation and deffered compilation

--- lsp-protocol.el
+++ lsp-protocol.el
@@ -41,7 +41,7 @@
     "Convert a KEYWORD to string."
     (substring (symbol-name keyword) 1))
 
-  (defvar lsp-use-plists nil))
+  (defvar lsp-use-plists (getenv "LSP_USE_PLISTS")))
 
 (defmacro lsp-interface (&rest interfaces)
   "Generate LSP bindings from INTERFACES triplet.
