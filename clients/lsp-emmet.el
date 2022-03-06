d5ce144275865b9fea6a846e80a7f1df934ce929
Seong Yong-ju <sei40kr@gmail.com>
2022-03-06 22:31:55 +0900

    Add emmet-ls support

---
+++ clients/lsp-emmet.el
@@ -0,0 +1,65 @@
+;;; lsp-emmet.el --- lsp-mode Emmet integration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 emacs-lsp maintainers
+
+;; Author: lsp-mode maintainers
+;; Keywords: lsp, emmet
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; LSP Client for Emmet
+
+;;; Code:
+
+(require 'lsp-mode)
+
+;;; emmet-ls
+(defgroup lsp-emmet-ls nil
+  "Settings for emmet-ls."
+  :group 'lsp-mode
+  :link '(url-link "https://github.com/aca/emmet-ls")
+  :package-version '(lsp-mode . "8.0.1"))
+
+(defcustom lsp-emmet-ls-command '("emmet-ls" "--stdio")
+  "The command that starts emmet-ls."
+  :type '(repeat :tag "List of string values" string)
+  :group 'lsp-emmet-ls
+  :package-version '(lsp-mode . "8.0.1"))
+
+(lsp-dependency 'emmet-ls
+                '(:system "emmet-ls")
+                '(:npm :package "emmet-ls"
+                       :path "emmet-ls"))
+
+(lsp-register-client
+ (make-lsp-client
+  :new-connection (lsp-stdio-connection
+                   (lambda ()
+                     `(,(or (executable-find (cl-first lsp-emmet-ls-command))
+                            (lsp-package-path 'emmet-ls))
+                       ,@(cl-rest lsp-emmet-ls-command))))
+  :activation-fn (lsp-activate-on "html" "css" "scss" "less" "javascriptreact" "typescriptreact")
+  :priority -1
+  :add-on? t
+  :multi-root t
+  :server-id 'emmet-ls
+  :download-server-fn (lambda (_client callback error-callback _update?)
+                        (lsp-package-ensure 'emmet-ls callback error-callback))))
+
+(lsp-consistency-check lsp-emmet)
+
+(provide 'lsp-emmet)
+;;; lsp-emmet.el ends here
--- lsp-mode.el
+++ lsp-mode.el
@@ -175,13 +175,13 @@ As defined by the Language Server Protocol 3.16."
 (defcustom lsp-client-packages
   '(ccls lsp-actionscript lsp-ada lsp-angular lsp-ansible lsp-bash lsp-beancount lsp-clangd lsp-clojure
          lsp-cmake lsp-crystal lsp-csharp lsp-css lsp-d lsp-dart lsp-dhall lsp-docker lsp-dockerfile
-         lsp-elm lsp-elixir lsp-erlang lsp-eslint lsp-fortran lsp-fsharp lsp-gdscript lsp-go lsp-graphql
-         lsp-hack lsp-grammarly lsp-groovy lsp-haskell lsp-haxe lsp-idris lsp-java lsp-javascript lsp-json
-         lsp-kotlin lsp-latex lsp-ltex lsp-lua lsp-markdown lsp-nginx lsp-nim lsp-nix lsp-magik
+         lsp-elm lsp-elixir lsp-emmet lsp-erlang lsp-eslint lsp-fortran lsp-fsharp lsp-gdscript lsp-go
+         lsp-graphql lsp-hack lsp-grammarly lsp-groovy lsp-haskell lsp-haxe lsp-idris lsp-java lsp-javascript
+         lsp-json lsp-kotlin lsp-latex lsp-ltex lsp-lua lsp-markdown lsp-nginx lsp-nim lsp-nix lsp-magik
          lsp-metals lsp-mssql lsp-ocaml lsp-pascal lsp-perl lsp-php lsp-pwsh lsp-pyls lsp-pylsp
          lsp-pyright lsp-python-ms lsp-purescript lsp-r lsp-remark lsp-rf lsp-rust lsp-solargraph
          lsp-sorbet lsp-sourcekit lsp-sonarlint lsp-tailwindcss lsp-tex lsp-terraform lsp-toml
-         lsp-ttcn3 lsp-typeprof lsp-v lsp-vala lsp-verilog lsp-vetur lsp-volar lsp-vhdl lsp-vimscript 
+         lsp-ttcn3 lsp-typeprof lsp-v lsp-vala lsp-verilog lsp-vetur lsp-volar lsp-vhdl lsp-vimscript
          lsp-xml lsp-yaml lsp-sqls lsp-svelte lsp-steep lsp-zig)
   "List of the clients to be automatically required."
   :group 'lsp-mode
