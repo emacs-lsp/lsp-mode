9a5750b53ed90dbde3fb86c8f566e47e4500e46d
Alan Zimmerman <alan.zimm@gmail.com>
2022-07-22 17:38:59 +0100

    Add client configuration for Marksman (#3632)
    https://github.com/artempyanykh/marksman
    
    Emacs config:
    
        (use-package markdown-mode
          :after lsp-mode
          :hook (markdown-mode . lsp)
          :config
          (require 'lsp-marksman))

---
+++ clients/lsp-marksman.el
@@ -0,0 +1,95 @@
+;;; lsp-marksman.el --- lsp-mode marksman integration -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021 lsp-mode maintainers
+
+;; Author: lsp-mode maintainers
+;; Keywords: languages
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;  LSP client for marksman
+
+;;; Code:
+
+(require 'lsp-mode)
+
+;;; Marksman
+(defgroup lsp-marksman nil
+  "Settings for the marksman language server client."
+  :group 'lsp-mode
+  :link '(url-link "https://github.com/artempyanykh/marksman")
+  :package-version '(lsp-mode . "8.0.0"))
+
+(defcustom lsp-marksman-server-command "marksman"
+  "The binary (or full path to binary) which executes the server."
+  :type 'string
+  :group 'lsp-marksman
+  :package-version '(lsp-mode . "8.0.0"))
+
+(defcustom lsp-marksman-server-command-args '()
+  "Command-line arguments for the marksman lsp server. Not normally Needed."
+  :type '(repeat 'string)
+  :group 'lsp-marksman
+  :package-version '(lsp-mode . "8.0.0"))
+
+(defcustom lsp-marksman-download-url
+  (format "https://github.com/artempyanykh/marksman/releases/latest/download/%s"
+          (pcase system-type
+            ('gnu/linux "marksman-linux")
+            ('darwin (if (string-match "^aarch64-.*" system-configuration)
+                         "marksman-macos" ;; Note: probably wrong/will not work
+                       "marksman-macos"))
+            ('windows-nt "marksman.exe")))
+  "Automatic download url for Marksman."
+  :type 'string
+  :group 'lsp-marksman
+  :package-version '(lsp-mode . "8.0.0"))
+
+(defcustom lsp-marksman-store-path (f-join lsp-server-install-dir
+                                                "marksman"
+                                                (if (eq system-type 'windows-nt)
+                                                    "marksman.exe"
+                                                  "marksman"))
+  "The path to the file in which `marksman' will be stored."
+  :type 'file
+  :group 'lsp-marksman
+  :package-version '(lsp-mode . "8.0.0"))
+
+(lsp-dependency 'marksman
+                '(:system "marksman")
+                `(:download :url lsp-marksman-download-url
+                            :store-path lsp-marksman-store-path
+                            :set-executable? t))
+
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection
+                                   (lambda ()
+                                     (cons (or (executable-find lsp-marksman-server-command)
+                                               (lsp-package-path 'marksman))
+                                           lsp-marksman-server-command-args)))
+                  :activation-fn (lsp-activate-on "markdown")
+                  :initialized-fn (lambda (workspace)
+                                    (with-lsp-workspace workspace
+                                      (lsp--set-configuration (lsp-configuration-section "marksman"))))
+                  :priority -1
+                  :server-id 'marksman
+                  :download-server-fn (lambda (_client callback error-callback _update?)
+                                        (lsp-package-ensure 'marksman callback error-callback))))
+
+(lsp-consistency-check lsp-marksman)
+
+(provide 'lsp-marksman)
+;;; lsp-marksman.el ends here
--- lsp-mode.el
+++ lsp-mode.el
@@ -177,7 +177,7 @@ As defined by the Language Server Protocol 3.16."
          lsp-cmake lsp-crystal lsp-csharp lsp-css lsp-d lsp-dart lsp-dhall lsp-docker lsp-dockerfile
          lsp-elm lsp-elixir lsp-emmet lsp-erlang lsp-eslint lsp-fortran lsp-fsharp lsp-gdscript lsp-go
          lsp-gleam lsp-graphql lsp-hack lsp-grammarly lsp-groovy lsp-haskell lsp-haxe lsp-idris lsp-java lsp-javascript
-         lsp-json lsp-kotlin lsp-latex lsp-ltex lsp-lua lsp-markdown lsp-nginx lsp-nim lsp-nix lsp-magik
+         lsp-json lsp-kotlin lsp-latex lsp-ltex lsp-lua lsp-markdown lsp-marksman lsp-nginx lsp-nim lsp-nix lsp-magik
          lsp-metals lsp-mssql lsp-ocaml lsp-openscad lsp-pascal lsp-perl lsp-perlnavigator lsp-php lsp-pwsh lsp-pyls lsp-pylsp
          lsp-pyright lsp-python-ms lsp-purescript lsp-r lsp-remark lsp-rf lsp-rust lsp-solargraph
          lsp-sorbet lsp-sourcekit lsp-sonarlint lsp-tailwindcss lsp-tex lsp-terraform lsp-toml
@@ -1884,7 +1884,7 @@ regex in IGNORED-FILES."
     lsp-clojure lsp-cmake lsp-crystal lsp-csharp lsp-css lsp-d lsp-dhall
     lsp-dockerfile lsp-elixir lsp-elm lsp-erlang lsp-eslint lsp-fortran lsp-fsharp lsp-gdscript
     lsp-go lsp-gleam lsp-graphql lsp-groovy lsp-hack lsp-haxe lsp-html lsp-idris lsp-javascript lsp-json lsp-kotlin lsp-lua
-    lsp-markdown lsp-nginx lsp-nim lsp-nix lsp-ocaml lsp-perl lsp-perlnavigator lsp-php lsp-prolog lsp-purescript lsp-pwsh
+    lsp-markdown lsp-marksman lsp-nginx lsp-nim lsp-nix lsp-ocaml lsp-perl lsp-perlnavigator lsp-php lsp-prolog lsp-purescript lsp-pwsh
     lsp-pyls lsp-pylsp lsp-racket lsp-r lsp-rf lsp-rust lsp-solargraph lsp-sorbet lsp-sqls
     lsp-steep lsp-svelte lsp-terraform lsp-tex lsp-toml lsp-ttcn3 lsp-typeprof lsp-v lsp-vala lsp-verilog
     lsp-vetur lsp-volar lsp-vhdl lsp-vimscript lsp-xml lsp-yaml lsp-zig)
