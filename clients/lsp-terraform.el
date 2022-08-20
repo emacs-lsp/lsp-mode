b16cbe4b605152421251b0e5439fb90b7044fc06
Sibi Prabakaran <sibi@psibi.in>
2022-08-20 12:16:55 +0530

    terraform: Implement new command to get discovered versions

--- clients/lsp-terraform.el
+++ clients/lsp-terraform.el
@@ -222,6 +222,20 @@ This is a synchronous action."
      :no-wait nil
      :no-merge t))
 
+(defun lsp-terraform-ls-version ()
+  "Get information about the terraform binary version for the current module."
+  (interactive)
+  (let* ((terraform-data (lsp-request
+                          "workspace/executeCommand"
+                          (list :command "terraform-ls.module.terraform"
+                                :arguments (vector (format "uri=%s" (lsp--path-to-uri (lsp-workspace-root))))
+                                )
+                          :no-wait nil
+                          :no-merge nil)))
+    (message "Required: %s, Current: %s"
+             (lsp:terraform-ls-module-terraform-required-version terraform-data)
+             (lsp:terraform-ls-module-terraform-discovered-version terraform-data))))
+
 (lsp-consistency-check lsp-terraform)
 
 (defvar treemacs-position)
