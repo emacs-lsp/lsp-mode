68bdac0f806789c064856a47d0e413b65a260e1e
brotzeit <brotzeitmacher@gmail.com>
2022-09-15 20:22:55 +0200

    only display inlay hints when label is string

--- clients/lsp-rust.el
+++ clients/lsp-rust.el
@@ -1031,13 +1031,14 @@ meaning."
            (-let* (((&rust-analyzer:InlayHint :position :label :kind :padding-left :padding-right) hint)
                    (pos (lsp--position-to-point position))
                    (overlay (make-overlay pos pos nil 'front-advance 'end-advance)))
-             (overlay-put overlay 'lsp-rust-analyzer-inlay-hint t)
-             (overlay-put overlay 'before-string
-                          (format "%s%s%s"
-                                  (if padding-left " " "")
-                                  (propertize (lsp-rust-analyzer-format-inlay label kind)
-                                              'font-lock-face (lsp-rust-analyzer-face-for-inlay kind))
-                                  (if padding-right " " ""))))))
+             (when (stringp label)
+               (overlay-put overlay 'lsp-rust-analyzer-inlay-hint t)
+               (overlay-put overlay 'before-string
+                            (format "%s%s%s"
+                                    (if padding-left " " "")
+                                    (propertize (lsp-rust-analyzer-format-inlay label kind)
+                                                'font-lock-face (lsp-rust-analyzer-face-for-inlay kind))
+                                    (if padding-right " " "")))))))
        :mode 'tick))
   nil)
 
