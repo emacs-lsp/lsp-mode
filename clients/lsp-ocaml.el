853cad81500bbb401d832f624fcabd9a885ea386
Mattias <5543639+mattiasdrp@users.noreply.github.com>
2022-11-21 19:48:08 +0100

    Allow to not cut the signature received on hover (#3637)
    * Allow to not cut the signature received on hover
    
    Some servers (i.e. the ocamllsp one) return only the
    signature on hover (without the documentation) but tend to put newlines between
    the different items. With the current implementation of
    `lsp-clients-extract-signature-on-hover`, the displayed signature will
    only contain the first item
    
    ```
    config1:string list ->
    config2:string list -> config3:string list -> int -> int -> int
    ```
    
    will then be rendered as
    
    ```
    config1:string list ->
    ```
    
    With the new implementation, a custom variable allows to take control
    over this behaviour and the function has been reimplemented to either
    return the previous result or return the signature with newlines
    replaced by spaces
    
    * Three modes, default, space and "newline"
    
    The third mode is only reachable if you use the function on your
    own (I use it to kill the returned type since this is a feature that
    exists in the previous OCaml mode I used)
    
    Thanks to @yyoncho I moved my code to the lsp-ocaml.el file to make it
    OCaml related. The ocaml-lsp server returns a markdown file with the
    following content:
    
    ```ocaml
    <type>
    ---
    <doc>
    ```
    
    so it's easy to split it to only keep the signature.
    
    If the second mode is chosen the type will be edited to be displayed
    on one line and end with `...` if it is too wide
    
    * Small updates and better function documentation
    
    * Update lsp-mode.el

--- clients/lsp-ocaml.el
+++ clients/lsp-ocaml.el
@@ -77,6 +77,57 @@
   :priority 0
   :server-id 'ocaml-lsp-server))
 
+(defcustom lsp-cut-signature 'space
+  "If non-nil, signatures returned on hover will not be split on newline."
+  :group 'lsp-ocaml
+  :type '(choice (symbol :tag "Default behaviour" 'cut)
+                 (symbol :tag "Display all the lines with spaces" 'space)))
+
+(cl-defmethod lsp-clients-extract-signature-on-hover (contents (_server-id (eql ocamllsp)) &optional storable)
+  "Extract a representative line from OCaml's CONTENTS, to show in the echo area.
+This function splits the content between the signature
+and the documentation to display the signature
+and truncate it if it's too wide.
+The STORABLE argument is used if you want to use this
+function to get the type and, for example, kill and yank it.
+
+An example of function using STORABLE is:
+
+  (defun mdrp/lsp-get-type-and-kill ()
+    (interactive)
+    (let ((contents (-some->> (lsp--text-document-position-params)
+                    (lsp--make-request \"textDocument/hover\")
+                    (lsp--send-request)
+                    (lsp:hover-contents))))
+      (let ((contents (and contents
+                    (lsp--render-on-hover-content
+                     contents
+                     t))))
+        (let ((contents
+               (pcase (lsp-workspaces)
+                 (`(,workspace)
+                  (lsp-clients-extract-signature-on-hover
+                   contents
+                   (lsp--workspace-server-id workspace)
+                   t))
+                 (lsp-clients-extract-signature-on-hover
+                   contents
+                   nil)
+                 )))
+          (message \"Copied %s to kill-ring\" contents)
+          (kill-new contents)))))"
+  (let ((type (s-trim (lsp--render-element (lsp-make-marked-string
+                                            :language "ocaml"
+                                            :value (car (s-split "---" (lsp--render-element contents))))))))
+    (if (equal nil storable)
+        (if (eq lsp-cut-signature 'cut)
+            (car (s-lines type))
+          ;; else lsp-cut-signature is 'space
+          (let ((ntype (s-replace "\n" " " type)))
+            (if (>= (length ntype) (frame-width))
+                (concat (substring ntype 0 (- (frame-width) 4)) "...")
+              ntype)))
+      type)))
 
 (lsp-consistency-check lsp-ocaml)
 
