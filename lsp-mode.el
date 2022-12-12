98cc577df9bb777bed6e9644d174e5e6d0e9c989
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-13 02:37:28 +0800

    feature: Add command to uninstall server

--- lsp-mode.el
+++ lsp-mode.el
@@ -7784,6 +7784,16 @@ When prefix UPDATE? is t force installation even if the server is present."
     (lsp--install-server-internal chosen-client update?)))
 
 ;;;###autoload
+(defun lsp-uninstall-server (dir)
+  "Delete a LSP server from `lsp-server-install-dir'."
+  (interactive
+   (list (read-directory-name "Uninstall LSP server: " (f-slash lsp-server-install-dir) nil t)))
+  (unless (file-directory-p dir)
+    (user-error "Couldn't find %S directory" dir))
+  (delete-directory dir 'recursive)
+  (message "Uninstalled %S" (file-name-nondirectory dir)))
+
+;;;###autoload
 (defun lsp-update-server (&optional server-id)
   "Interactively update (reinstall) a server."
   (interactive)
