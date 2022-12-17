4688de7f33acde41adb9b79bc9a631599ff6a92b
Ivan Yonchovski <yyoncho@users.noreply.github.com>
2022-12-17 20:15:49 +0200

    Revert "Fix CI (#3859)" (#3862)
    This reverts commit 8c568d0cef1b0145291edac1f79f3907c4014685.

--- lsp-dired.el
+++ lsp-dired.el
@@ -72,11 +72,12 @@
                    (if (dired-move-to-filename nil)
                        (let* ((file (dired-get-filename nil t))
                               (bol (progn
+                                     (point-at-bol)
                                      (search-forward-regexp "^[[:space:]]*" (line-end-position) t)
                                      (point)))
                               (face (lsp-dired--face-for-path file)))
                          (when face
-                           (-doto (make-overlay bol (line-end-position))
+                           (-doto (make-overlay bol (point-at-eol))
                              (overlay-put 'evaporate t)
                              (overlay-put 'face face))))
                      (cl-return-from :file nil))
@@ -88,26 +89,22 @@
 
 (defface lsp-dired-path-error-face
   '((t :underline (:style wave :color "Red1")))
-  "Face used for breadcrumb paths on headerline when there is an error under
-that path"
+  "Face used for breadcrumb paths on headerline when there is an error under that path"
   :group 'lsp-dired)
 
 (defface lsp-dired-path-warning-face
   '((t :underline (:style wave :color "Yellow")))
-  "Face used for breadcrumb paths on headerline when there is an warning under
-that path"
+  "Face used for breadcrumb paths on headerline when there is an warning under that path"
   :group 'lsp-dired)
 
 (defface lsp-dired-path-info-face
   '((t :underline (:style wave :color "Green")))
-  "Face used for breadcrumb paths on headerline when there is an info under that
-path"
+  "Face used for breadcrumb paths on headerline when there is an info under that path"
   :group 'lsp-dired)
 
 (defface lsp-dired-path-hint-face
   '((t :underline (:style wave :color "Green")))
-  "Face used for breadcrumb paths on headerline when there is an hint under that
-path"
+  "Face used for breadcrumb paths on headerline when there is an hint under that path"
   :group 'lsp-dired)
 
 (defun lsp-dired--face-for-path (dir)
