emacs-lsp/lsp-mode

commit 16b13adf857f9cd2e7d48a1bfe3c0b1ed4107339

headerline: Speed up root detection (#3912)

lsp-headerline.el
@@ -250,7 +250,7 @@ PATH is the current folder to be checked."
 
 (defun lsp-headerline--build-project-string ()
   "Build the project-segment string for the breadcrumb."
-  (-if-let (root (lsp-workspace-root))
+  (-if-let (root (lsp-headerline--workspace-root))
       (propertize (lsp-headerline--directory-with-action
                    root
                    (f-filename root))
@@ -304,7 +304,7 @@ PATH is the current folder to be checked."
 
 (defun lsp-headerline--build-path-up-to-project-string ()
   "Build the path-up-to-project segment for the breadcrumb."
-  (if-let ((root (lsp-workspace-root)))
+  (if-let ((root (lsp-headerline--workspace-root)))
       (let ((segments (or
                        lsp-headerline--path-up-to-project-segments
                        (setq lsp-headerline--path-up-to-project-segments
@@ -412,6 +412,10 @@ PATH is the current folder to be checked."
 (defun lsp-headerline--disable-breadcrumb ()
   "Disable headerline breadcrumb mode."
   (lsp-headerline-breadcrumb-mode -1))
+
+(defun lsp-headerline--workspace-root ()
+  (--when-let (car lsp--buffer-workspaces)
+    (lsp--workspace-root it)))
 
 ;;;###autoload
 (define-minor-mode lsp-headerline-breadcrumb-mode

commit f42ef9206323e4e12a76a4b2405cb1520acc994f

fix[headerline]: use lsp-workspace-root to avoid issues with multiple active workspaces
Commit 16b13adf857f9cd2e7d48a1bfe3c0b1ed4107339 introduced a new function lsp-headerline--workspace-root aimed at speeding up the headerline update function, as lsp-workspace-root is expensive (https://github.com/emacs-lsp/lsp-mode/issues/3911)

Unfortunally, the strategy assumed that the buffer-local lsp--buffer-workspace would only contain workspaces that the current buffer belongs to. This assumption appears to be wrong.

As a result, when two buffers belonging to different workspaces (/path/to/workspace1 and /path/to/workspace2) had the headerline active, the idle function on the workspace2 buffer could actually assume it's workspace root was /path/to/workspace1. 

As suggested by @yyoncho in https://github.com/emacs-lsp/lsp-mode/commit/16b13adf857f9cd2e7d48a1bfe3c0b1ed4107339#r98855365, we can cache the result of lsp-workspace-root and have the faster headerline introduced by @erikarvstedt and avoid the bug altogether.

lsp-headerline.el
@@ -144,6 +144,9 @@ is an hints in symbols range."
   "Holds the current breadcrumb path-up-to-project segments for
 caching purposes.")
 
+(defvar-local lsp-headerline--cached-workspace-root nil
+  "Holds the current value of lsp-workspace-root for caching purposes")
+
 ;; Redefine local vars of `all-the-icons' to avoid bytecode compilation errors.
 (defvar all-the-icons-default-adjust)
 (defvar all-the-icons-scale-factor)
@@ -420,8 +423,10 @@ PATH is the current folder to be checked."
   (lsp-headerline-breadcrumb-mode -1))
 
 (defun lsp-headerline--workspace-root ()
-  (--when-let (car lsp--buffer-workspaces)
-    (lsp--workspace-root it)))
+  (when (null lsp-headerline--cached-workspace-root)
+    (setq lsp-headerline--cached-workspace-root (lsp-workspace-root)))
+
+  lsp-headerline--cached-workspace-root)
 
 ;;;###autoload
 (define-minor-mode lsp-headerline-breadcrumb-mode