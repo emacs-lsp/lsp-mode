ec1c8349e859e5db66e5fb0744943e37b090745f
Rodrigo Kassick <kassick@users.noreply.github.com>
2023-02-02 09:30:11 -0300

    refactor: make lsp-headerline--workspace-root more concise
    Co-authored-by: erikarvstedt <36110478+erikarvstedt@users.noreply.github.com>

--- lsp-headerline.el
+++ lsp-headerline.el
@@ -423,10 +423,8 @@ PATH is the current folder to be checked."
   (lsp-headerline-breadcrumb-mode -1))
 
 (defun lsp-headerline--workspace-root ()
-  (when (null lsp-headerline--cached-workspace-root)
+(or lsp-headerline--cached-workspace-root
     (setq lsp-headerline--cached-workspace-root (lsp-workspace-root)))
-
-  lsp-headerline--cached-workspace-root)
 
 ;;;###autoload
 (define-minor-mode lsp-headerline-breadcrumb-mode
