c09a6c692d2501a3b1c644a33d24303e149a39b0
actondev <chris.actondev@gmail.com>
2023-06-18 20:01:49 +0200

    [nim] configurable nimlsp executable like nimlangserver

--- clients/lsp-nim.el
+++ clients/lsp-nim.el
@@ -77,8 +77,15 @@
   :group 'lsp-nim
   :package-version '(lsp-mode . "8.0.1"))
 
+(defcustom lsp-nim-lsp "nimlsp"
+  "Path to `nimlsp'"
+  :type 'number
+  :group 'lsp-nim
+  :package-version '(lsp-mode . "8.0.1"))
+
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection "nimlsp")
+ (make-lsp-client :new-connection (lsp-stdio-connection
+                                   (lambda () lsp-nim-lsp))
                   :activation-fn (lsp-activate-on "nim")
                   :priority -1
                   :server-id 'nimlsp))
