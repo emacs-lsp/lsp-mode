7d5db4f54bf2107585c145f5e7f163ead7228357
boskamp <lambert@boskamp-consulting.com>
2023-10-27 07:53:52 +0200

    Lsp volar activate (#3962)
    * Enable Volar to start when no workspace root exists yet
    
    * Re-indent lsp-volar--activate-p

--- clients/lsp-volar.el
+++ clients/lsp-volar.el
@@ -99,14 +99,14 @@ in the WORKSPACE-ROOT."
 (defun lsp-volar--activate-p (filename &optional _)
   "Check if the volar-language-server should be enabled base on FILENAME."
   (if lsp-volar-take-over-mode
-      (and (or
-            (and (lsp-workspace-root) (lsp-volar--vue-project-p (lsp-workspace-root)))
-            (and (lsp-workspace-root) lsp-volar-activate-file (f-file-p (f-join (lsp-workspace-root) lsp-volar-activate-file))))
-           (or (or (string-match-p "\\.mjs\\|\\.[jt]sx?\\'" filename)
-                   (and (derived-mode-p 'js-mode 'typescript-mode 'typescript-ts-mode)
-                        (not (derived-mode-p 'json-mode))))
-               (string= (file-name-extension filename) "vue")))
-   (string= (file-name-extension filename) "vue")))
+      (or (or
+           (and (lsp-workspace-root) (lsp-volar--vue-project-p (lsp-workspace-root)))
+           (and (lsp-workspace-root) lsp-volar-activate-file (f-file-p (f-join (lsp-workspace-root) lsp-volar-activate-file))))
+          (or (or (string-match-p "\\.mjs\\|\\.[jt]sx?\\'" filename)
+                  (and (derived-mode-p 'js-mode 'typescript-mode 'typescript-ts-mode)
+                       (not (derived-mode-p 'json-mode))))
+              (string= (file-name-extension filename) "vue")))
+    (string= (file-name-extension filename) "vue")))
 
 (lsp-register-client
  (make-lsp-client
