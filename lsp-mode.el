6fc45a57f1c0a8d58ea0fc0dd87d46c94506517d
Ruin0x11 <ipickering2@gmail.com>
2023-12-27 23:15:07 -0600

    Request pull diagnostics when file is opened

--- lsp-mode.el
+++ lsp-mode.el
@@ -4299,6 +4299,8 @@ yet."
 
   (lsp-managed-mode 1)
 
+  (lsp-diagnostics--request-pull-diagnostics lsp--cur-workspace)
+
   (run-hooks 'lsp-after-open-hook)
   (when-let ((client (-some-> lsp--cur-workspace (lsp--workspace-client))))
     (-some-> (lsp--client-after-open-fn client)
