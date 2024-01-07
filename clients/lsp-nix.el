81c2984ee01e96195f4b8eba61ee46c4d9c9fa1b
Peter Becich <peterbecich@gmail.com>
2024-01-07 10:01:39 -0800

    suggestion: use `nixd` language server, priority over `rnix` (#4288)
    * use `nixd` language server, priority over `rnix`
    
    `nixd` is more actively maintained than `rnix`
    https://github.com/nix-community/nixd
    https://github.com/nix-community/rnix-lsp
    
    https://github.com/emacs-lsp/lsp-mode/issues/4168
    
    * nixd documentation
    
    https://github.com/nix-community/nixd/blob/main/nixd/docs/user-guide.md#installation

--- clients/lsp-nix.el
+++ clients/lsp-nix.el
@@ -41,6 +41,23 @@
  (make-lsp-client :new-connection (lsp-stdio-connection (lambda () lsp-nix-rnix-server-path))
                   :major-modes '(nix-mode nix-ts-mode)
                   :server-id 'rnix-lsp
+                  :priority -2))
+
+(defgroup lsp-nix-nixd nil
+  "LSP support for Nix, using nixd language server."
+  :group 'lsp-mode
+  :link '(url-link "https://github.com/nix-community/nixd"))
+
+(defcustom lsp-nix-nixd-server-path "nixd"
+  "Executable path for the server."
+  :group 'lsp-nix-nixd
+  :type 'string
+  :package-version '(lsp-mode . "8.0.0"))
+
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection (lambda () lsp-nix-nixd-server-path))
+                  :major-modes '(nix-mode)
+                  :server-id 'nixd-lsp
                   :priority -1))
 
 (defgroup lsp-nix-nil nil
