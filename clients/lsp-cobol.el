d7b4a825b534604b59706787b1914aaffd399734
JenChieh <jcs090218@gmail.com>
2024-03-17 15:30:40 -0700

    feat(lsp-cobol): Add automatic install language service capability

--- clients/lsp-cobol.el
+++ clients/lsp-cobol.el
@@ -32,29 +32,131 @@
   :link '(url-link "https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol")
   :package-version `(lsp-mode . "8.0.1"))
 
+(defcustom lsp-cobol-server-path nil
+  "Path points for COBOL language service.
+
+This is only for development use."
+  :type 'string
+  :group 'lsp-cobol)
+
 (defcustom lsp-cobol-port 1044
   "Port to connect server to."
   :type 'integer
   :group 'lsp-cobol)
 
-(defun lsp-cobol--tcp-connect-to-port ()
-  "Define a TCP connection to language server."
-  (list
-   :connect (lambda (filter sentinel name _environment-fn _workspace)
-              (let* ((host "localhost")
-                     (port lsp-cobol-port)
-                     (tcp-proc (lsp--open-network-stream host port (concat name "::tcp"))))
+;;
+;;; Installation
 
-                (set-process-query-on-exit-flag tcp-proc nil)
-                (set-process-filter tcp-proc filter)
-                (set-process-sentinel tcp-proc sentinel)
-                (cons tcp-proc tcp-proc)))
-   :test? (lambda () t)))
+(defmacro lsp-cobol--mute-apply (&rest body)
+  "Execute BODY without message."
+  (declare (indent 0) (debug t))
+  `(let (message-log-max)
+     (with-temp-message (or (current-message) nil)
+       (let ((inhibit-message t)) ,@body))))
+
+(defun lsp-cobol--execute (cmd &rest args)
+  "Return non-nil if CMD executed succesfully with ARGS."
+  (save-window-excursion
+    (lsp-cobol--mute-apply
+      (= 0 (shell-command (concat cmd " "
+                                  (mapconcat #'shell-quote-argument
+                                             (cl-remove-if #'null args)
+                                             " ")))))))
+
+;;
+;;; Installation
+
+(defcustom lsp-cobol-server-store-path
+  (expand-file-name "cobol/" lsp-server-install-dir)
+  "The path to the file in which COBOL language service will be stored."
+  :type 'file
+  :group 'lsp-cobol)
+
+(defcustom lsp-cobol-server-version "2.1.1"
+  "The COBOL language service version to install."
+  :type 'file
+  :group 'lsp-cobol)
+
+(defconst lsp-cobol-download-url-format
+  "https://github.com/eclipse-che4z/che-che4z-lsp-for-cobol/releases/download/%s/cobol-language-support-%s-%s-%s%s.vsix"
+  "Format to the download url link.")
+
+(defun lsp-cobol--server-url ()
+  "Return Url points to the cobol language service's zip/tar file."
+  (let* ((x86 (string-prefix-p "x86_64" system-configuration))
+         (arch (if x86 "x64" "arm64"))
+         (version lsp-cobol-server-version))
+    (cl-case system-type
+      ((cygwin windows-nt ms-dos)
+       (format lsp-cobol-download-url-format
+               version "win32" arch version "-signed"))
+      (darwin
+       (format lsp-cobol-download-url-format
+               version "darwin" arch version ""))
+      (gnu/linux
+       (format lsp-cobol-download-url-format
+               version "linux" arch version "")))))
+
+(defvar lsp-cobol--server-download-url (lsp-cobol--server-url)
+  "The actual url used to download language server.")
+
+(defvar lsp-cobol--downloaded-file (f-join lsp-cobol-server-store-path "temp.tar")
+  "The full file path after downloading the server zipped file.")
+
+(defun lsp-cobol--extract-compressed-file ()
+  "Install COBOL language service."
+  (cond ((file-exists-p lsp-cobol--downloaded-file)
+         ;; Suprisingly, you can just use `tar' to unzip a zip file on Windows.
+         ;; Therefore, just use the same command.
+         (lsp-cobol--execute "tar" "-xvzf" lsp-cobol--downloaded-file "-C" lsp-cobol-server-store-path)
+         ;; Delete the zip file.
+         (ignore-errors (delete-file lsp-cobol--downloaded-file)))
+        (t
+         (error "Can't extract the downloaded file: %s" lsp-cobol--downloaded-file))))
+
+(defun lsp-cobol--stored-executable ()
+  "Return the stored COBOL language service executable."
+  (executable-find
+   (f-join lsp-cobol-server-store-path
+           (concat "extension/server/native/"
+                   (cl-case system-type
+                     ((cygwin windows-nt ms-dos) "engine.exe")
+                     (darwin                     "server-mac")
+                     (gnu/linux                  "server-linux"))))))
+
+(lsp-dependency
+ 'cobol-language-service
+ '(:system "cobol-language-service")
+ `(:download :url ,lsp-cobol--server-download-url
+             :store-path ,lsp-cobol--downloaded-file))
+
+;;
+;;; Core
+
+(defun lsp-cobol--executable ()
+  "Return the COBOL language service executable."
+  (or lsp-cobol-server-path
+      (lsp-cobol--stored-executable)))
+
+(defun lsp-cobol-server-start-fun (&rest _)
+  "Define COOBL language service start function."
+  `(,(lsp-cobol--executable)))
+
+(defun lsp-cobol--tcp-connect-to-port ()
+  "Define a TCP connection to language service."
+  (let ((lsp--tcp-port lsp-cobol-port))
+    (lsp-tcp-connection #'lsp-cobol-server-start-fun)))
 
 (lsp-register-client
- (make-lsp-client :new-connection (lsp-cobol--tcp-connect-to-port)
-                  :activation-fn (lsp-activate-on "cobol")
-                  :server-id 'cobol))
+ (make-lsp-client
+  :new-connection #'lsp-cobol--tcp-connect-to-port
+  :activation-fn (lsp-activate-on "cobol")
+  :priority -1
+  :server-id 'cobol-ls
+  :download-server-fn
+  (lambda (_client _callback error-callback _update?)
+    (lsp-package-ensure 'cobol-language-service
+                        #'lsp-cobol--extract-compressed-file error-callback))))
 
 (lsp-consistency-check lsp-cobol)
 
