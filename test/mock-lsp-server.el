24fbb325f6d8cdeb54e461625ebbdad8bd61574f
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-07-17 18:39:53 +0200

    Working lsp server mock

--- test/mock-lsp-server.el
+++ test/mock-lsp-server.el
@@ -2,36 +2,38 @@
 ;; -*- lexical-binding: t; -*-
 ;; -*- coding: utf-8; -*-
 
-(defun greeting (id)
-  (format "Content-Length: 778
-Content-Type: application/vscode-jsonrpc; charset=utf8
+(defun json-rpc-string (body)
+  ;; 1+ - extra new-line at the end
+  (format "Content-Length: %d\r\nContent-Type: application/vscode-jsonrpc; charset=utf8\r\n\r\n%s\n" (1+ (string-bytes body)) body))
 
-{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":{\"capabilities\":{\"codeActionProvider\":true,\"codeLensProvider\":{\"resolveProvider\":false},\"completionProvider\":{\"resolveProvider\":true,\"triggerCharacters\":[\".\"]},\"documentFormattingProvider\":true,\"documentHighlightProvider\":true,\"documentRangeFormattingProvider\":true,\"documentSymbolProvider\":true,\"definitionProvider\":true,\"executeCommandProvider\":{\"commands\":[]},\"hoverProvider\":true,\"referencesProvider\":true,\"renameProvider\":true,\"foldingRangeProvider\":true,\"signatureHelpProvider\":{\"triggerCharacters\":[\"(\",\",\",\"=\"]},\"textDocumentSync\":{\"change\":2,\"save\":{\"includeText\":true},\"openClose\":true},\"workspace\":{\"workspaceFolders\":{\"supported\":true,\"changeNotifications\":true}},\"experimental\":{}},\"serverInfo\":{\"name\":\"pylsp\",\"version\":\"1.3.3\"}}}" id))
+(defun greeting (id)
+  (json-rpc-string
+   (format "{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":{\"capabilities\":{\"codeActionProvider\":true,\"codeLensProvider\":{\"resolveProvider\":false},\"completionProvider\":{\"resolveProvider\":true,\"triggerCharacters\":[\".\"]},\"documentFormattingProvider\":true,\"documentHighlightProvider\":true,\"documentRangeFormattingProvider\":true,\"documentSymbolProvider\":true,\"definitionProvider\":true,\"executeCommandProvider\":{\"commands\":[]},\"hoverProvider\":true,\"referencesProvider\":true,\"renameProvider\":true,\"foldingRangeProvider\":true,\"signatureHelpProvider\":{\"triggerCharacters\":[\"(\",\",\",\"=\"]},\"textDocumentSync\":{\"change\":2,\"save\":{\"includeText\":true},\"openClose\":true},\"workspace\":{\"workspaceFolders\":{\"supported\":true,\"changeNotifications\":true}},\"experimental\":{}},\"serverInfo\":{\"name\":\"mockS\",\"version\":\"1.3.3\"}}}"
+           id)))
 
 (defun ack (id)
-  (format "Content-Length: 37
-Content-Type: application/vscode-jsonrpc; charset=utf8
-
-{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":[]}" id))
+  (json-rpc-string (format "{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":[]}" id)))
 
 (defun shutdown-ack (id)
-  (format "Content-Length: 37
-Content-Type: application/vscode-jsonrpc; charset=utf8
+  (json-rpc-string (format "{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":null}" id)))
 
-{\"jsonrpc\":\"2.0\",\"id\":%d,\"result\":null}" id))
-
-(defvar diagnostics "Content-Length: 645
-Content-Type: application/vscode-jsonrpc; charset=utf8
-
-{\"jsonrpc\":\"2.0\",\"method\":\"textDocument\\/publishDiagnostics\",\"params\":{\"uri\":\"file:\\/\\/\\/home\\/necto\\/proj\\/lsp-sonarlint\\/fixtures\\/sample.py\",\"diagnostics\":[{\"source\":\"flake8\",\"code\":\"F821\",\"range\":{\"start\":{\"line\":2,\"character\":3},\"end\":{\"line\":2,\"character\":18}},\"message\":\"F821 undefined name 'true'\",\"severity\":2},{\"source\":\"flake8\",\"code\":\"F821\",\"range\":{\"start\":{\"line\":2,\"character\":11},\"end\":{\"line\":2,\"character\":18}},\"message\":\"F821 undefined name 'false'\",\"severity\":2},{\"source\":\"flake8\",\"code\":\"F701\",\"range\":{\"start\":{\"line\":3,\"character\":4},\"end\":{\"line\":3,\"character\":10}},\"message\":\"F701 'break' outside loop\",\"severity\":2}]}}")
-
+(defun diagnostics (for-file)
+  (json-rpc-string
+   (format "{\"jsonrpc\":\"2.0\",\"method\":\"textDocument\\/publishDiagnostics\",\"params\":{\"uri\":\"%s\",\"diagnostics\":[{\"source\":\"flake8\",\"code\":\"F821\",\"range\":{\"start\":{\"line\":2,\"character\":3},\"end\":{\"line\":2,\"character\":18}},\"message\":\"F821 undefined name 'true'\",\"severity\":2},{\"source\":\"flake8\",\"code\":\"F821\",\"range\":{\"start\":{\"line\":2,\"character\":11},\"end\":{\"line\":2,\"character\":18}},\"message\":\"F821 undefined name 'false'\",\"severity\":2},{\"source\":\"flake8\",\"code\":\"F701\",\"range\":{\"start\":{\"line\":3,\"character\":4},\"end\":{\"line\":3,\"character\":10}},\"message\":\"F701 'broke' outside loop\",\"severity\":2}]}}"
+           for-file)))
 
 (defun get-id (input)
-  (if (string-match "id\":\\([0-9]+\\)" input)
+  (if (string-match "\"id\":\\([0-9]+\\)" input)
       (string-to-number (match-string 1 input))
     nil))
 
-;; Echo
+(setq ll "{\"jsonrpc\":\"2.0\",\"method\":\"textDocument/didOpen\",\"params\":{\"textDocument\":{\"uri\":\"file:///home/necto/proj/lsp-mode/sample.awk\",\"languageId\":\"awk\",\"version\":0,\"text\":\"heyho! Hi I'm a new member here. I'm a 16 year old\\nline 1 is here and here\\nline 2 is here and here\\n\"}}}")
+
+(defun get-file-path (input)
+  (if (string-match "\"uri\":\"\\(file:\\/\\/[^,]+\\)\"," input)
+      (match-string 1 input)
+    nil))
+
 (let (line stopped)
   (while (and (not stopped) (setq line (read-string "")))
     (cond
@@ -45,7 +47,7 @@ Content-Type: application/vscode-jsonrpc; charset=utf8
      ((string-match "method\":\"shutdown" line)
       (princ (shutdown-ack (get-id line))))
      ((string-match "didOpen" line)
-      (princ diagnostics))
+      (princ (diagnostics (get-file-path line))))
      ((string-match "method\":\"workspace/didChangeConfiguration" line)
       ;; No need to acknowledge
       )
