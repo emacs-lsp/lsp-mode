58d58ea454158c9a65b55c696dec815d02f74063
Arseniy Zaostrovnykh <necto.ne@gmail.com>
2024-07-19 22:14:26 +0200

    Doc strings for the mock server test

--- test/lsp-mock-server-test.el
+++ test/lsp-mock-server-test.el
@@ -34,18 +34,27 @@
 (require 'lsp-mode)
 
 ;; Taken from lsp-integration-tests.el
-(defconst lsp-test-location (file-name-directory (or load-file-name buffer-file-name)))
+(defconst lsp-test-location (file-name-directory (or load-file-name buffer-file-name))
+  "Directory of the tests containing mock server and fixtures.")
 
 (defconst lsp-test-mock-server-location
-  (expand-file-name "mock-lsp-server.el" lsp-test-location))
+  (expand-file-name "mock-lsp-server.el" lsp-test-location)
+  "Path to the mock server script.")
 
 (defconst lsp-test-mock-server-command-file
-  (expand-file-name "mock-server-commands.el" lsp-test-location))
+  (expand-file-name "mock-server-commands.el" lsp-test-location)
+  "File mock server reads commands from.")
 
 (defconst lsp-test-sample-file
-  (f-join lsp-test-location "fixtures/SamplesForMock/sample.txt"))
+  (f-join lsp-test-location "fixtures/SamplesForMock/sample.txt")
+  "The sample file used to conduct tests upon.")
 
 (defun lsp-test-send-command-to-mock-server (command)
+  "Pass the given COMMAND to the mock server.
+
+It uses the pre-configured file to write command to, then sends a
+notification to the server so that it looks into the file and
+executes the command."
   ;; Can run only one command at a time
   (should (not (file-exists-p lsp-test-mock-server-command-file)))
   (write-region command nil lsp-test-mock-server-command-file nil nil nil 'excl)
@@ -53,6 +62,7 @@
   (lsp-notify "$/setTrace" '(:value "messages")))
 
 (defun register-mock-client ()
+  "Register mock client that spawns the mock server."
   (lsp-register-client
    (make-lsp-client
     :new-connection (lsp-stdio-connection
@@ -61,10 +71,12 @@
     :priority 100
     :server-id 'mock-server)))
 
-(defun lsp-test-total-server-count ()
+(defun lsp-test-total-folder-count ()
+  "Count total number of active root folders in the session."
   (hash-table-count (lsp-session-folder->servers (lsp-session))))
 
 (defun lsp-test--find-line (file-content line)
+  "Find LINE in the multi-line FILE-CONTENT string."
   (let ((lines (split-string file-content "\n"))
         (line-number 0)
         (found nil))
@@ -78,6 +90,20 @@
     found))
 
 (defun lsp-test-diag-make (file-content line marker)
+  "Create a single-line diagnostics range summary.
+
+Find LINE in FILE-CONTENT and take that as the line number.
+Set the :from and :to characters to reflect the position of
+`^^^^' in the MARKER.
+
+Example (suppose line #3 of current buffer is \"full line\"):
+
+(lsp-test-diag-make (buffer-string)
+                    \"full line\"
+                    \"     ^^^^\")
+
+-> (:line 3 :from 5 :to 8)
+"
   (let ((line-number (lsp-test--find-line file-content line)))
     (should-not (null line-number))
     (should (eq (length marker) (length line)))
@@ -85,6 +111,10 @@
     (list :line line-number :from (match-beginning 1) :to (match-end 1))))
 
 (defun lsp-test-diag-get (diagnostic)
+  "Get the single-line diagnostics range summary of DIAGNOSTIC.
+
+DIAGNOSTIC must have a single-line range.
+Returns its range converted to `(:line .. :from .. :to ..)' format."
   (let* ((range (ht-get diagnostic "range"))
          (start (ht-get range "start"))
          (end (ht-get range "end")))
@@ -94,6 +124,14 @@
           :to (ht-get end "character"))))
 
 (defun lsp-test-make-diagnostics (for-file contents forbidden-word)
+  "Come up with a diagnostic highlighting FORBIDDEN-WORD.
+
+Scan CONTENTS for FORBIDDEN-WORD and produce diagnostics for each occurence.
+Returns a p-list compatible with the mock server.
+
+FOR-FILE is the path to the file to include in the diagnostics.
+It might not contain exactly CONTENTS because it the diagnostic
+might be generated for a modified and not saved buffer content."
   (with-temp-buffer
     (insert contents)
     (goto-char (point-min))
@@ -112,15 +150,26 @@
       `(:path ,for-file :diags ,diagnostics))))
 
 (defun lsp-test-command-send-diags (file-path file-contents forbidden-word)
+  "Generate and command the mock server to publish diagnostics.
+
+Command the mock server to publish diagnostics highlighting every occurence of
+FORBIDDEN-WORD in FILE-CONTENTS that corresponds to FILE-PATH."
   (let ((diags (lsp-test-make-diagnostics file-path file-contents forbidden-word)))
     (lsp-test-send-command-to-mock-server
      (format "(publish-diagnostics '%s)"
              (prin1-to-string diags)))))
 
 (defun lsp-test-crash-server-with-message (message)
+  "Command the mock server to crash with MESSAGE."
   (lsp-test-send-command-to-mock-server (format "(error %S)" message)))
 
+;; I could not figure out how to use lsp-test-wait safely
+;; (e.g., aborting it after a failed test), so I use a simpler
+;; version.
 (defun lsp-test--sync-wait-for (condition-func)
+  "Synchronously waiting for CONDITION-FUNC to return non-nil.
+
+Returns the non-nil return value of CONDITION-FUNC."
   (let ((result (funcall condition-func)))
     (while (not result)
       (sleep-for 0.05)
@@ -128,16 +177,21 @@
     result))
 
 (defmacro lsp-test-sync-wait (condition)
+  "Wait for the CONDITION to become non-nil and return it."
   `(lsp-test--sync-wait-for (lambda () ,condition)))
 
 (defun lsp-mock--run-with-mock-server (test-body)
+  "Run TEST-BODY function with mock LSP client connected to the mock server.
+
+This is an environment function that configures lsp-mode, mock lsp-client,
+opens the `lsp-test-sample-file' and starts the mock server."
   (let ((lsp-clients (lsp-ht)) ; clear all clients
         (lsp-diagnostics-provider :none) ; focus on LSP itself, not its UI integration
         (lsp-restart 'ignore) ; Avoid restarting the server or prompting user on a crash
         (lsp-enable-snippet nil) ; Avoid warning that lsp-yasnippet is not intalled
         (lsp-warn-no-matched-clients nil) ; Mute warning LSP can't figure out src lang
         (workspace-root (file-name-directory lsp-test-sample-file))
-        (initial-server-count (lsp-test-total-server-count)))
+        (initial-server-count (lsp-test-total-folder-count)))
     (register-mock-client) ; register mock client as the one an only lsp client
     (lsp-workspace-folders-add workspace-root)
     (let* ((buf (find-file-noselect lsp-test-sample-file)))
@@ -147,7 +201,7 @@
               (prog-mode)
               (lsp)
               ;; Make sure the server started
-              (should (eq (lsp-test-total-server-count) (1+ initial-server-count)))
+              (should (eq (lsp-test-total-folder-count) (1+ initial-server-count)))
               (lsp-test-sync-wait (eq 'initialized
                                       (lsp--workspace-status (cl-first (lsp-workspaces)))))
               (funcall test-body)))
@@ -160,9 +214,13 @@
             (delete-file lsp-test-mock-server-command-file))))
     (with-timeout (5 (error "LSP mock server refuses to stop"))
       ;; Make sure the server stopped
-      (lsp-test-sync-wait (= initial-server-count (lsp-test-total-server-count))))))
+      (lsp-test-sync-wait (= initial-server-count (lsp-test-total-folder-count))))))
 
 (defmacro lsp-mock-run-with-mock-server (&rest test-body)
+  "Evaluate TEST-BODY in the context of a mock client connected to mock server.
+
+Opens the `lsp-test-sample-file' and initiates the LSP session.
+TEST-BODY can interact with the mock server."
   `(lsp-mock--run-with-mock-server (lambda () ,@test-body)))
 
 (ert-deftest lsp-mock-server-reports-issues ()
@@ -178,13 +236,14 @@
                                       "                   ^^^^^^^         ")))))
 
 (ert-deftest lsp-mock-server-crashes ()
-  (let ((initial-serv-count (lsp-test-total-server-count)))
+  "Test that the mock server crashes when instructed so."
+  (let ((initial-serv-count (lsp-test-total-folder-count)))
     (when-let ((buffer (get-buffer "*mock-server::stderr*")))
       (kill-buffer buffer))
     (lsp-mock-run-with-mock-server
-     (should (eq (lsp-test-total-server-count) (1+ initial-serv-count)))
+     (should (eq (lsp-test-total-folder-count) (1+ initial-serv-count)))
      (lsp-test-crash-server-with-message "crashed by command")
-     (lsp-test-sync-wait (eq initial-serv-count (lsp-test-total-server-count)))
+     (lsp-test-sync-wait (eq initial-serv-count (lsp-test-total-folder-count)))
      (let ((buffer (get-buffer "*mock-server::stderr*")))
        (should buffer)
        (with-current-buffer buffer
@@ -193,6 +252,7 @@
          (goto-char (point-max)))))))
 
 (defun lsp-mock-get-first-diagnostic-line ()
+  "Get the line number of the first diagnostic on `lsp-test-sample-file'."
   (let ((diags (gethash lsp-test-sample-file (lsp-diagnostics t))))
     (when diags
       (let* ((diag (car diags))
@@ -201,6 +261,7 @@
         (ht-get start "line")))))
 
 (ert-deftest lsp-mock-server-updates-diagnostics ()
+  "Test that mock server can update diagnostics and lsp-mode reflects that."
   (lsp-mock-run-with-mock-server
    ;; There are no diagnostics at first
    (should (eq (length (gethash lsp-test-sample-file (lsp-diagnostics t))) 0))
@@ -231,6 +292,12 @@
                                       "                   ^^^^^           ")))))
 
 (ert-deftest lsp-mock-server-updates-diags-with-delay ()
+  "Test demonstrating delay in the diagnostics update.
+
+If server takes noticeable time to update diagnostics after a
+document change, and `lsp-diagnostic-clean-after-change' is
+nil (default), diagnostic ranges will be off until server
+publishes the update. This test demonstrates this behavior."
   (lsp-mock-run-with-mock-server
    ;; There are no diagnostics at first
    (should (eq (length (gethash lsp-test-sample-file (lsp-diagnostics t))) 0))
