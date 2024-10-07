762094ca3268e125ec707655926a2cc074d419f4
Edmund Miller <git@edmundmiller.dev>
2024-10-07 11:13:35 -0500

    fix(nextflow): Rework install logic

--- clients/lsp-nextflow.el
+++ clients/lsp-nextflow.el
@@ -31,13 +31,53 @@
 (defgroup lsp-nextflow nil
   "LSP support for nextflow, using nextflow-language-server."
   :group 'lsp-mode
-  :link '(url-link "https://github.com/nextflowLanguageServer/nextflow-language-server"))
+  :link '(url-link "https://github.com/nextflow-io/vscode-language-nextflow"))
 
 (defcustom lsp-nextflow-server-file (f-join lsp-server-install-dir "nextflow-language-server-all.jar")
   "JAR file path for nextflow-language-server-all.jar."
   :group 'lsp-nextflow
   :risky t
   :type 'file)
+
+;;
+;;; Installation
+
+(defcustom lsp-nextflow-server-store-path
+  (expand-file-name "nextflow/" lsp-server-install-dir)
+  "The path to the file in which COBOL language service will be stored."
+  :type 'file
+  :group 'lsp-nextflow)
+
+(defcustom lsp-nextflow-server-version "1.0.0"
+  "The Nextflow language server version to install."
+  :type 'file
+  :group 'lsp-nextflow)
+
+;; TODO Use  (lsp-vscode-extension-url "Nightrains" "robloxlsp" "1.5.11")
+(defcustom lsp-nextflow-server-download-url
+  "https://github.com/edmundmiller/vscode-language-nextflow/releases/download/1.0.0/nextflow-1.0.0.vsix"
+  "Format to the download url link.")
+
+
+;; TODO
+;; (defun lsp-lua-roblox-language-server-test ()
+;;   "Test Lua language server binaries and files."
+;;   (and (f-exists? lsp-lua-roblox-language-server-main-location)
+;;        (f-exists? lsp-lua-roblox-language-server-bin)))
+
+(defun lsp-nextflow-language-server-install (_client callback error-callback _update?)
+  "Download the latest version of nextflow-language-server and extract it"
+  (lsp-download-install
+   (lambda (&rest _)
+     (set-file-modes lsp-nextflow-language-server-bin #o0700)
+     (funcall callback))
+   error-callback
+   :url lsp-nextflow-server-download-url
+   :store-path lsp-nextflow-server-store-path))
+
+(defun lsp-nextflow--stored-executable ()
+  "Return the stored Nextflow language service executable."
+  (f-join lsp-cobol-server-store-path "extension/bin/language-server-all.jar"))
 
 (defun lsp-nextflow--lsp-command ()
   "Generate LSP startup command."
