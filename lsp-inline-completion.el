d3ff223b750c26698a1658261214112b75bcc180
Rodrigo Kassick <kassick@gmail.com>
2024-11-25 13:13:05 -0300

    fix: wrong number of arguments

--- lsp-inline-completion.el
+++ lsp-inline-completion.el
@@ -267,7 +267,7 @@
 
     ;; If a template, format it -- keep track of the end position!
     (when (eq kind 'snippet)
-      (let ((end-marker (set-marker (make-marker))))
+      (let ((end-marker (set-marker (make-marker) (point))))
         (lsp--expand-snippet (buffer-substring text-insert-start text-insert-end)
                              text-insert-start
                              text-insert-end)
