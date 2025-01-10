a34f70c9ad19163e80dc4cd9e7056db6168abb5f
Samuel Kyletoft <samuel@kyletoft.se>
2025-01-10 21:55:52 +0100

    Feature: Add support for Roc in roc-ts-mode

--- lsp-mode.el
+++ lsp-mode.el
@@ -185,7 +185,7 @@ As defined by the Language Server Protocol 3.16."
      lsp-mojo lsp-move lsp-mssql lsp-nextflow lsp-nginx lsp-nim lsp-nix lsp-nushell lsp-ocaml
      lsp-openscad lsp-pascal lsp-perl lsp-perlnavigator lsp-php lsp-pls
      lsp-purescript lsp-pwsh lsp-pyls lsp-pylsp lsp-pyright lsp-python-ms
-     lsp-qml lsp-r lsp-racket lsp-remark lsp-rf lsp-roslyn lsp-rubocop lsp-ruby-lsp
+     lsp-qml lsp-r lsp-racket lsp-remark lsp-rf lsp-roc lsp-roslyn lsp-rubocop lsp-ruby-lsp
      lsp-ruby-syntax-tree lsp-ruff lsp-rust lsp-semgrep lsp-shader
      lsp-solargraph lsp-solidity lsp-sonarlint lsp-sorbet lsp-sourcekit
      lsp-sql lsp-sqls lsp-steep lsp-svelte lsp-tailwindcss lsp-terraform
@@ -943,6 +943,7 @@ Changes take effect only when a new session is started."
     (perl-mode . "perl")
     (cperl-mode . "perl")
     (robot-mode . "robot")
+    (roc-ts-mode . "roc")
     (racket-mode . "racket")
     (nix-mode . "nix")
     (nix-ts-mode . "nix")
