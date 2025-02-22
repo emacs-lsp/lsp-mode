222de5b5f35ff3cfa0e380d19dd5e17eeaf9fe17
JenChieh <jcs090218@gmail.com>
2025-02-22 10:26:52 -0800

    chore: Support for message-mode with LTEX-ls

--- lsp-mode.el
+++ lsp-mode.el
@@ -988,6 +988,7 @@ Changes take effect only when a new session is started."
     (meson-mode . "meson")
     (yang-mode . "yang")
     (matlab-mode . "matlab")
+    (message-mode . "plaintext")
     (mu4e-compose-mode . "plaintext"))
   "Language id configuration.")
 
