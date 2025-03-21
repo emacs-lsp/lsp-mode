87ffe4700b7364fe9a338f964cce9407c82a2c57
mattiasdrp <mattias@kojin.tech>
2025-03-22 00:53:48 +0100

    OCaml: Remove previous function that allowed to retrieve only the signature
    This function was useful because textDocument/hover returned the type and
    documentation of the identifier at point making it hard to kill the type only
    
    Now that ocamllsp/typeEnclosing is used instead there is no reason to keep this
    poorly written function (I was young and innocent)

--- clients/lsp-ocaml.el
+++ clients/lsp-ocaml.el
@@ -104,52 +104,6 @@
   :group 'lsp-ocaml-server
   :type 'boolean)
 
-(cl-defmethod lsp-clients-extract-signature-on-hover (contents (_server-id (eql ocaml-lsp-server)) &optional storable)
-  "Extract a representative line from OCaml's CONTENTS, to show in the echo area.
-This function splits the content between the signature
-and the documentation to display the signature
-and truncate it if it's too wide.
-The STORABLE argument is used if you want to use this
-function to get the type and, for example, kill and yank it.
-
-An example of function using STORABLE is:
-
-  (defun mdrp/lsp-get-type-and-kill ()
-    (interactive)
-    (let ((contents (-some->> (lsp--text-document-position-params)
-                    (lsp--make-request \"textDocument/hover\")
-                    (lsp--send-request)
-                    (lsp:hover-contents))))
-      (let ((contents (and contents
-                    (lsp--render-on-hover-content
-                     contents
-                     t))))
-        (let ((contents
-               (pcase (lsp-workspaces)
-                 (`(,workspace)
-                  (lsp-clients-extract-signature-on-hover
-                   contents
-                   (lsp--workspace-server-id workspace)
-                   t))
-                 (lsp-clients-extract-signature-on-hover
-                   contents
-                   nil)
-                 )))
-          (message \"Copied %s to kill-ring\" contents)
-          (kill-new contents)))))"
-  (let ((type (s-trim (lsp--render-element (lsp-make-marked-string
-                                            :language "ocaml"
-                                            :value (car (s-split "---" (lsp--render-element contents))))))))
-    (if (equal nil storable)
-        (if (eq lsp-cut-signature 'cut)
-            (car (s-lines type))
-          ;; else lsp-cut-signature is 'space
-          (let ((ntype (s-replace "\n" " " type)))
-            (if (>= (length ntype) (frame-width))
-                (concat (substring ntype 0 (- (frame-width) 4)) "...")
-              ntype)))
-      type)))
-
 ;;; -------------------
 ;;; OCaml-lsp faces
 ;;; -------------------
