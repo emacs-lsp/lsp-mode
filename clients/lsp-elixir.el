79c5637a6686f3080d371f32f1f5023228acf579
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2025-08-05 08:13:45 +0200

    Bump elixir-ls version to v0.29.0
    The download url also changed and updated in this commit.

--- clients/lsp-elixir.el
+++ clients/lsp-elixir.el
@@ -123,7 +123,7 @@ Leave as default to let `executable-find' search for it."
   :type '(repeat string)
   :package-version '(lsp-mode . "8.0.0"))
 
-(defcustom lsp-elixir-ls-version "v0.26.4"
+(defcustom lsp-elixir-ls-version "v0.29.0"
   "Elixir-Ls version to download.
 It has to be set before `lsp-elixir.el' is loaded and it has to
 be available here: https://github.com/elixir-lsp/elixir-ls/releases/"
@@ -132,7 +132,7 @@ be available here: https://github.com/elixir-lsp/elixir-ls/releases/"
   :package-version '(lsp-mode . "9.0.0"))
 
 (defcustom lsp-elixir-ls-download-url
-  (format "https://github.com/elixir-lsp/elixir-ls/releases/download/%1$s/elixir-ls-%1$s.zip"
+  (format "https://github.com/elixir-lsp/elixir-ls/releases/download/%1$s/elixir-ls.zip"
           lsp-elixir-ls-version)
   "Automatic download url for elixir-ls."
   :type 'string
