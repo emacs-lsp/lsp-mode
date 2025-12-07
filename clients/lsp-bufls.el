5541e584065e4cc553cc61462a1b37e49ac70243
Norio Suzuki <norio.suzuki@gmail.com>
2025-12-08 07:45:55 +0900

    lsp-buf: change buf CLI args
    - The lsp command in buf has graduated from beta
    - No longer times out by default

--- clients/lsp-bufls.el
+++ clients/lsp-bufls.el
@@ -71,7 +71,7 @@
   :link '(url-lint "https://github.com/bufbuild/buf")
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-buf-args `("beta" "lsp" "--timeout" "0" "--log-format" "json")
+(defcustom lsp-buf-args `("lsp" "serve")
   "Arguments to pass to buf CLI."
   :type '(repeat string)
   :group 'lsp-buf
