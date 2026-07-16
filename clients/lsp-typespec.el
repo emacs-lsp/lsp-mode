abdc173447cc42042befc7293b479079488dd77a
Jeremy Meng <jeremy.ymeng@gmail.com>
2026-07-16 10:17:06 -0700

    defer resolution of `lsp-typespec--server-executable-path`

--- clients/lsp-typespec.el
+++ clients/lsp-typespec.el
@@ -63,7 +63,7 @@
  (make-lsp-client
   :semantic-tokens-faces-overrides '(:types (("docCommentTag" . font-lock-keyword-face)
                                              ("event" . default)))
-  :new-connection (lsp-stdio-connection `(,(lsp-typespec--server-executable-path) "--stdio"))
+  :new-connection (lsp-stdio-connection (lambda () (list (lsp-typespec--server-executable-path) "--stdio")))
   :activation-fn (lsp-activate-on "typespec")
   :major-modes '(typespec-mode typespec-ts-mode)
   :server-id 'typespec-lsp
@@ -78,8 +78,9 @@
                               (lsp-find-workspace 'typespec-lsp (buffer-file-name)))))
     (--each (lsp--workspace-buffers workspace)
       (when (lsp-buffer-live-p it)
-        (lsp-with-current-buffer it
-          (lsp-semantic-tokens--enable))))))
+        (lsp-with-current-buffer
+         it
+         (lsp-semantic-tokens--enable))))))
 
 (with-eval-after-load 'typespec
   (when lsp-semantic-tokens-enable
