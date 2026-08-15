emacs-lsp/lsp-mode

commit 564b526704c44443d6cff5a589bbe6ad021e46ca

Bump the magik language server to 0.10.0 (#4514)
* [lsp-magik] Add extra settings, re-order and remove obsolete setting

* Update lsp-magik.el

* Update lsp-magik.el

clients/lsp-magik.el
@@ -34,7 +34,7 @@
   :tag "Lsp Magik"
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-magik-version "0.9.0"
+(defcustom lsp-magik-version "0.10.0"
   "Version of LSP server."
   :type `string
   :group `lsp-magik
@@ -58,14 +58,14 @@
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-magik-smallworld-gis nil
-  "Path to Smallworld Core."
+(defcustom lsp-magik-java-home nil
+  "Path to Java Runtime, Java 17 minimum."
   :type `string
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-magik-libs-dirs []
-  "Paths to libs dirs of Smallworld products."
+(defcustom lsp-magik-product-dirs []
+  "Paths to (compiled, containing a libs/ directory) products."
   :type `lsp-string-vector
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
@@ -82,8 +82,8 @@
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-magik-typing-show-atom-inlay-hints nil
-  "Show atom type inlay hints."
+(defcustom lsp-magik-typing-show-typing-inlay-hints nil
+  "Show typing inlay hints."
   :type `boolean
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
@@ -100,8 +100,32 @@
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
 
-(defcustom lsp-magik-typing-index-usages t
-  "Enable indexing of usages of globals/methods/slots/conditions by methods."
+(defcustom lsp-magik-typing-index-global-usages t
+  "Enable indexing of usages of globals by methods."
+  :type `boolean
+  :group `lsp-magik
+  :package-version '(lsp-mode . "9.0.0"))
+
+(defcustom lsp-magik-typing-index-method-usages nil
+  "Enable indexing of usages of methods by methods."
+  :type `boolean
+  :group `lsp-magik
+  :package-version '(lsp-mode . "9.0.0"))
+
+(defcustom lsp-magik-typing-index-slot-usages t
+  "Enable indexing of usages of slots by methods."
+  :type `boolean
+  :group `lsp-magik
+  :package-version '(lsp-mode . "9.0.0"))
+
+(defcustom lsp-magik-typing-index-condition-usages t
+  "Enable indexing of usages of conditions by methods."
+  :type `boolean
+  :group `lsp-magik
+  :package-version '(lsp-mode . "9.0.0"))
+
+(defcustom lsp-magik-typing-cache-indexed-definitions-method-usages t
+  "Store and load the indexed definitions in the workspace folders."
   :type `boolean
   :group `lsp-magik
   :package-version '(lsp-mode . "9.0.0"))
@@ -134,14 +158,18 @@
   :server-id 'magik))
 
 (lsp-register-custom-settings
- `(("magik.smallworldGis" lsp-magik-smallworld-gis)
-   ("magik.libsDirs" lsp-magik-libs-dirs)
+ `(("magik.javaHome" lsp-magik-java-home)
+   ("magik.productDirs" lsp-magik-product-dirs)
    ("magik.lint.overrideConfigFile" lsp-magik-lint-override-config-file)
    ("magik.typing.typeDatabasePaths" lsp-magik-typing-type-database-paths)
-   ("magik.typing.showAtomInlayHints" lsp-magik-typing-show-atom-inlay-hints)
+   ("magik.typing.showTypingInlayHints" lsp-magik-typing-show-typing-inlay-hints)
    ("magik.typing.showArgumentInlayHints" lsp-magik-typing-show-argument-inlay-hints)
    ("magik.typing.enableChecks" lsp-magik-typing-enable-checks)
-   ("magik.typing.indexUsages" lsp-magik-typing-index-usages)))
+   ("magik.typing.indexGlobalUsages" lsp-magik-typing-index-global-usages)
+   ("magik.typing.indexMethodUsages" lsp-magik-typing-index-method-usages)
+   ("magik.typing.indexSlotUsages" lsp-magik-typing-index-slot-usages)
+   ("magik.typing.indexConditionUsages" lsp-magik-typing-index-condition-usages)
+   ("magik.typing.cacheIndexedDefinitions" lsp-magik-typing-cache-indexed-definitions)))
 
 (lsp-consistency-check lsp-magik)
 

commit 342c4c019af9aa96b7f6eb73e7e1881feea41248

Rename lsp-magik-typing-cache-indexed-definitions-method-usages
The "-method-usages" suffix was a naming mistake from when this
setting was introduced in #4514: it maps to
magik.typing.cacheIndexedDefinitions and its docstring is about
caching indexed definitions in general, not method usages. The nil
default is kept: enabling it can write types.jsonl incorrectly if the
server is shut down uncleanly, a bug fixed on magik-tools' develop
branch but not yet released.

clients/lsp-magik.el
@@ -149,7 +149,7 @@ The next update resets the delay."
   :package-version '(lsp-mode . "10.0.0")
   :lsp-path "magik.typing.indexConditionUsages")
 
-(lsp-defcustom lsp-magik-typing-cache-indexed-definitions-method-usages nil
+(lsp-defcustom lsp-magik-typing-cache-indexed-definitions nil
   "Store and load the indexed definitions in the workspace folders."
   :type 'boolean
   :group 'lsp-magik